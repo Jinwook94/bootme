163404b3b871296fcb2d1cca64df335fbba80805
Jinwook Kim <kimjinwook94@gmail.com>
2022-09-14 15:22:05 +0900

    feat: 스프링부트 프로젝트 셋업

---
+++ backend/src/test/java/com/bootme/BootmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
