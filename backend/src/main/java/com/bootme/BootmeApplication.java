160e14242a406c6e767d6800bcb7521638375042
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2022-09-14 15:27:48 +0900

    feat: 스프링부트 프로젝트 셋업 (#2)

---
+++ backend/src/main/java/com/bootme/BootmeApplication.java
@@ -0,0 +1,14 @@
+package com.bootme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootmeApplication.class, args);
+    }
+
+}
+
