14842aebb343ebe753bd369fd8fd4b40496dc539
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2022-09-14 20:04:11 +0900

    feat: 리액트 프로젝트 셋업 (#4)
    * feat: package.json, 리액트 설치
    
    * feat: 프론트엔드 패키지 설치
    
    * feat: 프론트앤드 설정파일 작성 등

---
+++ frontend/webpack.common.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // tsx나 ts파일을 발견하면
+        use: ['babel-loader', 'ts-loader'], // 해당 loader를 통해서 버전을 변환
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
