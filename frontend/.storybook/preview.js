5f03e8077ec76f4083d3fbe3b6a75cd2c00447c1
Jinwook Kim <kimjinwook94@gmail.com>
2022-10-01 06:35:39 +0900

    chore:storybook 설정파일 수정

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,3 +1,10 @@
+import { ThemeProvider } from 'styled-components';
+import { MemoryRouter } from 'react-router';
+import GlobalStyle from '../src/styles/GlobalStyle';
+import { theme } from '../src/styles/theme';
+
+//참고: https://github.com/woowacourse-teams/2022-ternoko/blob/develop/frontend/.storybook/preview.js
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +14,14 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+      <MemoryRouter initialEntries={['/']}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Story />
+        </ThemeProvider>
+      </MemoryRouter>
+  ),
+];
