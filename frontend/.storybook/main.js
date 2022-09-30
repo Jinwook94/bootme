12f6a0f9d4b493051a51931fa4efb15821501b22
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2022-10-01 06:38:31 +0900

    chore:storybook 설정파일 수정 (#6)

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,6 +1,16 @@
+//참고: https://github.com/woowacourse-teams/2022-ternoko/blob/develop/frontend/.storybook/main.js
+
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
