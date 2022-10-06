cdcd1bab23b50ac7966d82afea6bd641300824d6
Jinwook Kim <kimjinwook94@gmail.com>
2022-10-07 03:11:13 +0900

    Button 컴포넌트 수정

--- frontend/src/components/@common/Button/Button.styles.ts
+++ frontend/src/components/@common/Button/Button.styles.ts
@@ -1,20 +1,46 @@
 import styled from "styled-components";
 
 export const ButtonStyle = styled.button`
-  width: fit-content;
-  padding: 0.75rem 1.4375rem;
-  border: none;
-  border-radius: 0.625rem;
-  background-color: ${(props) => props.theme.colors.BLUE_600};
-  font-size: 1.25rem;
-  font-weight: 500;
-  color: ${(props) => props.theme.colors.WHITE};
-  white-space: pre;
-  :hover {
-    opacity: 70%;
-  }
-  @media (max-width: 520px) {
-    padding: 0.625rem 0.75rem;
+    -webkit-text-size-adjust: 100%;
+    -webkit-tap-highlight-color: transparent;
+    --breakpoint-xs: 0;
+    --breakpoint-sm: 576px;
+    --breakpoint-md: 768px;
+    --breakpoint-lg: 992px;
+    --breakpoint-xl: 1200px;
+    --swiper-theme-color: #007aff;
+    --swiper-navigation-size: 44px;
+    word-break: keep-all;
+    word-wrap: break-word;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+    font: inherit;
+    margin: 0;
+    font-family: inherit;
+    overflow: visible;
+    text-transform: none;
+    background: none;
+    -webkit-appearance: button;
+    display: inline-block;
+    text-align: center;
+    vertical-align: middle;
+    user-select: none;
+    border-radius: 0.25rem;
+    border: 1px solid transparent;
+    font-weight: 500;
+    transition: color .08s ease-in-out, background-color .08s ease-in-out, border-color .08s ease-in-out, box-shadow .08s ease-in-out;
+    padding: 0.3125rem 0.8125rem;
     font-size: 0.875rem;
-  }
+    line-height: 1.25rem;
+    border-color: #0078FF;
+    background-color: #0078FF;
+    color: white;
+    max-height: 1.9375rem;
+    cursor: pointer;
+    
+    @media (max-width: 767px) {
+    padding: 0.3125rem 0.8125rem;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+}
 `;
