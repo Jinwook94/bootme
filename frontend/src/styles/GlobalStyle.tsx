f8aa9e0551825a41e224ed98a0e7bd8fd83a1062
Jinwook Kim <kimjinwook94@gmail.com>
2022-10-14 23:28:19 +0900

    style: global style 수정

--- frontend/src/styles/GlobalStyle.tsx
+++ frontend/src/styles/GlobalStyle.tsx
@@ -8,16 +8,38 @@ const GlobalStyle = createGlobalStyle`
       font-display: swap;
   }
     * {  
-      margin: 0;
-      padding:0 ;
-      box-sizing: border-box;
       font-family: 'Noto Sans KR', sans-serif;
+      margin: 0;
+      padding:0;
+      box-sizing: border-box;
+      font-size: 100%;
+      vertical-align: baseline;
+      color: inherit;
     } 
+    
+    html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
+      box-sizing: border-box;
+      padding: 0;
+      border: 0;
+      margin: 0;
+      font-size: 100%;
+      vertical-align: baseline;
+    }
+    
     a {
       text-decoration: none;
     } 
     body {
       max-width: 100%;
+      overflow-x: hidden;
+      font-display: 'block';
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      width: 100%;
+      margin: 0;
+      word-break: keep-all;
+      word-wrap: break-word;
+      color: #263747;
       overflow-x: hidden;
     }
     
