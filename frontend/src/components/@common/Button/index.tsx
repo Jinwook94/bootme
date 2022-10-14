75a1d8df80d44852501fcd59e7bf746899d8fb47
Jinwook Kim <kimjinwook94@gmail.com>
2022-10-14 23:21:39 +0900

    chore: Button 컴포넌트 수정(primary 속성 추가)

--- frontend/src/components/@common/Button/index.tsx
+++ frontend/src/components/@common/Button/index.tsx
@@ -2,19 +2,19 @@ import { ButtonStyle } from "./style";
 import React from "react";
 
 const Button = ({
-  type = 'button',
-  disabled = false,
-  color,
-  onClick,
-  ...props
-}: ButtonProps) => {
+                  type = 'button',
+                  disabled = false,
+                  color,
+                  onClick,
+                  ...props
+                }: ButtonProps) => {
   return (
-    <ButtonStyle disabled={disabled} onClick={onClick} color={color} {...props}>
-    </ButtonStyle>
+      <ButtonStyle disabled={disabled} onClick={onClick} color={color} {...props}>
+      </ButtonStyle>
   );
 };
 
-interface ButtonProps {
+export interface ButtonProps {
   type?: string;
   disabled?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
--- frontend/src/components/@common/Button/style.ts
+++ frontend/src/components/@common/Button/style.ts
@@ -1,46 +1,40 @@
 import styled from "styled-components";
+import {ButtonProps} from "./index"
 
-export const ButtonStyle = styled.button`
-    -webkit-text-size-adjust: 100%;
-    -webkit-tap-highlight-color: transparent;
-    --breakpoint-xs: 0;
-    --breakpoint-sm: 576px;
-    --breakpoint-md: 768px;
-    --breakpoint-lg: 992px;
-    --breakpoint-xl: 1200px;
-    --swiper-theme-color: #007aff;
-    --swiper-navigation-size: 44px;
-    word-break: keep-all;
-    word-wrap: break-word;
-    -webkit-font-smoothing: antialiased;
-    box-sizing: border-box;
-    font: inherit;
-    margin: 0;
-    font-family: inherit;
-    overflow: visible;
-    text-transform: none;
-    background: none;
-    -webkit-appearance: button;
+
+export const ButtonStyle = styled.button<ButtonProps>`
     display: inline-block;
+    padding: 0.4375rem 0.8125rem;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    border: 1px solid transparent;
+    background-color: transparent;
+    color: #0078FF;
+    cursor: pointer;
+    font-weight: 500;
     text-align: center;
     vertical-align: middle;
     user-select: none;
     border-radius: 0.25rem;
-    border: 1px solid transparent;
-    font-weight: 500;
     transition: color .08s ease-in-out, background-color .08s ease-in-out, border-color .08s ease-in-out, box-shadow .08s ease-in-out;
-    padding: 0.3125rem 0.8125rem;
-    font-size: 0.875rem;
-    line-height: 1.25rem;
-    border-color: #0078FF;
-    background-color: #0078FF;
-    color: white;
-    max-height: 1.9375rem;
-    cursor: pointer;
+    -webkit-appearance: button;
+    background: none;
+    overflow: visible;
+    
+    ${(p) => p.primary && `
+        border-color: #0078FF;
+        background-color: #0078FF;
+        color: white;
+    `}
+    
+    ${(p) => p.outlineLight && `
+        color: #f8f9fa;
+        border-color: #f8f9fa;
+    `}
     
     @media (max-width: 767px) {
-    padding: 0.3125rem 0.8125rem;
-    font-size: 0.875rem;
-    line-height: 1.25rem;
-}
+        padding: 0.3125rem 0.8125rem;
+        font-size: 0.875rem;
+        line-height: 1.25rem;
+    }
 `;
