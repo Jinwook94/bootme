3b79cf55f909a0e9b6255cdf2d637f9158863d47
Jinwook Kim <kimjinwook94@gmail.com>
2022-10-28 00:09:03 +0900

    feat: admin 리액트 프로젝트 생성 및 환경설정

---
+++ admin/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Button} from "react-bootstrap";
+
+const App = () => (
+    <>
+        <h1>React without CRA...</h1>
+        <Button> Bootstrap Button </Button>
+    </>
+);
+
+export default App;
