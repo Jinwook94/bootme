9d95dec1ac608526ae815fbdfc1d27913c72b4c7
Jinwook Kim <kimjinwook94@gmail.com>
2022-11-11 01:46:17 +0900

    chore: ErrorType 인터페이스 삭제

--- backend/src/main/java/com/bootme/common/exception/errortype/ErrorType.java
+++ backend/src/main/java/com/bootme/common/exception/errortype/ErrorType.java
@@ -1,15 +1,23 @@
 package com.bootme.common.exception.errortype;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-public interface ErrorType {
+import static org.springframework.http.HttpStatus.*;
 
-    String name();
+@Getter
+@RequiredArgsConstructor
+public enum ErrorType {
 
-    HttpStatus getHttpStatus();
+/*  Error Name               HTTP Status    Error Code     Error Message                  */
+    NOT_AUTHENTICATED       (UNAUTHORIZED, 1001, "인증에 실패했습니다."),
+    ALREADY_AUTHENTICATED   (UNAUTHORIZED, 1002, "이미 인증정보가 존재합니다."),
+    INVALID_TOKEN           (BAD_REQUEST,  1003, "유효하지 않은 토큰입니다."),
+    FORBIDDEN_REQUEST       (FORBIDDEN,    1004, "권한이 없습니다.");
 
-    int getErrorCode();
-
-    String getMessage();
+    final HttpStatus httpStatus;
+    final int errorCode;
+    final String message;
 
 }
