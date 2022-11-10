402eb3228248fc154b6988d2f575843c2027e5c3
Jinwook Kim <kimjinwook94@gmail.com>
2022-11-11 00:27:10 +0900

    docs: Spring Rest Docs 적용

---
+++ backend/src/test/java/com/bootme/util/ControllerTest.java
@@ -0,0 +1,54 @@
+package com.bootme.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.http.HttpDocumentation.httpRequest;
+import static org.springframework.restdocs.http.HttpDocumentation.httpResponse;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class ControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(final WebApplicationContext context, final RestDocumentationContextProvider provider) {
+        setMockMvcRestDocsSpec(context, provider);
+    }
+
+    private void setMockMvcRestDocsSpec(final WebApplicationContext context,
+                                        final RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(provider)
+                        .snippets()
+                        .withDefaults(httpRequest(), httpResponse())
+                        .and()
+                        .operationPreprocessors()
+                        .withRequestDefaults(
+                                modifyUris()
+                                        .scheme("https")
+                                        .host("api.bootMe.app")
+                                        .removePort(),
+                                prettyPrint()
+                        ).withResponseDefaults(
+                                removeHeaders(
+                                        "Transfer-Encoding",
+                                        "Date",
+                                        "Keep-Alive",
+                                        "Connection"),
+                                prettyPrint()
+                        )
+                ).build();
+    }
+}
