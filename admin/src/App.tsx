4a6c2bf0d0911a9264bf07370cfb20088fcfce22
Jinwook Kim <kimjinwook94@gmail.com>
2022-11-15 14:02:30 +0900

    chore: 기본 코드 삭제

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,37 +1,3 @@
-import React, {useState} from "react";
-import {Link, Routes, Route, useNavigate} from 'react-router-dom';
-// @ts-ignore
-import Modal from 'react-modal';
-import {PASSWORD} from "./constants/password";
-import AdminCourseCardPage from "./pages/AdminCourseCardPage";
-
-const App = () => {
-    const [password, setPassword] = useState()
-    const navigate = useNavigate();
-
-    const handleSubmit = (event: { preventDefault: () => void; }) => {
-        event.preventDefault();
-        if (password == PASSWORD) {
-            navigate('/courseCard');
-        }
-    };
-
-    const onChange = (e: any) => {
-        setPassword(e.target.value);
-    };
-
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <input type={"text"} placeholder={"입력하세요"} onChange={onChange} value={password}/>
-                <button type="submit">Submit</button>
-            </form>
-
-            <Routes>
-                <Route path="/courseCard" element={<AdminCourseCardPage />} />
-            </Routes>
-        </>
-    )
-}
+const App = () => {}
 
 export default App;
