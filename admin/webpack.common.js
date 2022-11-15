a32640fd3d55b265021af4d0bb6a6264290eac81
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2022-11-15 15:52:59 +0900

    feat: Admin 로그인 구현 (#22)
    * chore: 깃 캐시 클리어
    
    * chore: react-admin 라이브러리 설치
    
    * chore: 기본 코드 삭제
    
    * feat: Admin 로그인 클라이언트 구현 (react-admin)

--- admin/webpack.common.js
+++ admin/webpack.common.js
@@ -1,6 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
   entry: './src/index.tsx',
@@ -42,10 +45,15 @@ module.exports = {
     path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
     new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+       "process.env": JSON.stringify(dotenv.config().parsed),
+      }),
   ],
+
 };
