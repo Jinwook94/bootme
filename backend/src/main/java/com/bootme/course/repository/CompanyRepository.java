4bd1bee1925e20e6c10b24a749f278bd8ef1ac22
Jinwook Kim <kimjinwook94@gmail.com>
2022-11-21 23:19:00 +0900

    feat: Course, Company 리포지토리 구현

---
+++ backend/src/main/java/com/bootme/course/repository/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.bootme.course.repository;
+
+import com.bootme.course.domain.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
