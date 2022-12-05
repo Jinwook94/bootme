f14623f98373b483d894d7b7acead8da5d748856
Jinwook Kim <kimjinwook94@gmail.com>
2022-12-05 17:15:47 +0900

    feat: course DTO 수정

--- backend/src/main/java/com/bootme/course/dto/CourseRequest.java
+++ backend/src/main/java/com/bootme/course/dto/CourseRequest.java
@@ -1,44 +1,73 @@
 package com.bootme.course.dto;
 
-import com.bootme.course.domain.Company;
-import com.bootme.course.domain.Tag;
+import com.bootme.course.domain.*;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 public class CourseRequest {
 
-    @NotBlank(message = "url을 입력해주세요.")
-    private String url;
-
-    @NotBlank(message = "타이틀을 입력해주세요.")
+    @NotBlank(message = "커리큘럼 타이틀을 입력해주세요.")
     private String title;
 
-    private Company company;
+    @NotBlank(message = "커리큘럼 url을 입력해주세요.")
+    private String url;
+
+    @NotBlank(message = "커리큘럼 개설 회사를 입력해주세요")
+    private String companyName;
 
     @NotBlank(message = "장소를 입력해주세요.")
     private String location;
 
+    @NotNull(message = "커리큘럼 참여 비용을 입력해주세요.")
+    private int cost;
+
+    @NotBlank(message = "커리큘럼 비용 타입을 입력해주세요. - 무료, 무료국비, 유료")
+    private String costType;
+
+    private Dates dates;
+
+    @NotBlank(message = "강의 공간을 입해주세요 - 온라인, 오프라인, 온오프라인혼합")
+    private String onOffline;
+
     private List<Tag> tags;
+
+    @NotBlank(message = "선수 조건을 입력해주세요")
+    private String prerequisites;
+
+    @NotNull(message = "추천 커리큘럼 여부를 입력해주세요")
+    private boolean recommended;
+
+    @NotNull(message = "코딩테스트 진행 여부를 입력해주세요")
+    private boolean tested;
+
 
     public CourseRequest() {
     }
 
     @Builder
-    public CourseRequest(String url,
-                         String title,
-                         Company company,
-                         String location,
-                         List<Tag> tags) {
-        this.url = url;
+    public CourseRequest(String title, String url, String companyName,
+                         String location, int cost, String costType,
+                         Dates dates, String onOffline, List<Tag> tags,
+                         String prerequisites, boolean recommended, boolean tested) {
         this.title = title;
-        this.company = company;
+        this.url = url;
+        this.companyName = companyName;
         this.location = location;
+        this.cost = cost;
+        this.costType = costType;
+        this.dates = dates;
+        this.onOffline = onOffline;
         this.tags = tags;
+        this.prerequisites = prerequisites;
+        this.recommended = recommended;
+        this.tested = tested;
     }
 
 }
--- backend/src/main/java/com/bootme/course/dto/CourseResponse.java
+++ backend/src/main/java/com/bootme/course/dto/CourseResponse.java
@@ -1,7 +1,7 @@
 package com.bootme.course.dto;
 
-import com.bootme.course.domain.Company;
 import com.bootme.course.domain.Course;
+import com.bootme.course.domain.Dates;
 import com.bootme.course.domain.Tag;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,34 +12,59 @@ import java.util.List;
 public class CourseResponse {
 
     private Long id;
-    private String url;
     private String title;
-    private String company;
+    private String url;
+    private String companyName;
     private String location;
+    private int cost;
+    private String costType;
+    private Dates dates;
+    private String onOffline;
     private List<Tag> tags;
+    private String prerequisites;
+    private boolean recommended;
+    private boolean tested;
 
     public CourseResponse() {
     }
 
     @Builder
-    private CourseResponse(Long id, String url, String title, String company,
-                           String location, List<Tag> tags) {
+    public CourseResponse(Long id, String title, String url, String companyName,
+                          String location, int cost, String costType, Dates dates,
+                          String onOffline, List<Tag> tags, String prerequisites,
+                          boolean recommended, boolean tested) {
         this.id = id;
-        this.url = url;
         this.title = title;
-        this.company = company;
+        this.url = url;
+        this.companyName = companyName;
         this.location = location;
+        this.cost = cost;
+        this.costType = costType;
+        this.dates = dates;
+        this.onOffline = onOffline;
         this.tags = tags;
+        this.prerequisites = prerequisites;
+        this.recommended = recommended;
+        this.tested = tested;
     }
+
+
 
     public static CourseResponse of(Course course) {
         return CourseResponse.builder()
                 .id(course.getId())
-                .url(course.getUrl())
                 .title(course.getTitle())
-                .company(course.getCompany().getName())
+                .url(course.getUrl())
+                .companyName(course.getCompany().getName())
                 .location(course.getLocation())
+                .cost(course.getCost())
+                .costType(course.getCostType().name())
+                .dates(course.getDates())
+                .onOffline(course.getOnoffline().name())
                 .tags(course.getTags())
+                .prerequisites(course.getPrerequisites().name())
+                .recommended(course.isRecommended())
+                .tested(course.isTested())
                 .build();
     }
 }
