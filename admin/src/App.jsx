64fd429896800fc4f70b296edd0f0016c74c13bb
Jinwook Kim <kimjinwook94@gmail.com>
2022-12-09 21:21:28 +0900

    feat: 코스 리스트 페이지 구현 - CourseList

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,15 @@
 import * as React from "react";
-import { Admin } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import simpleRestProvider from 'ra-data-json-server';
 import authProvider from "./auth/authProvider";
-
+import courses from './courses';
 
 const dataProvider = simpleRestProvider('http://localhost:8080/admin')
 
 const App = () => (
     <Admin dataProvider={dataProvider} authProvider={authProvider}>
+        <Resource name="courses" {...courses} />
     </Admin>
-    );
+);
 
 export default App;
---
+++ admin/src/courses/CourseList.jsx
@@ -0,0 +1,40 @@
+import {
+    List,
+    Datagrid,
+    TextField,
+    BooleanField,
+    ChipField,
+    NumberField,
+    UrlField,
+    Pagination
+} from 'react-admin';
+
+const CoursePagination = props => <Pagination rowsPerPageOptions={[10, 25, 50, 100]} {...props} />;
+
+export const CourseList = (props) => (
+    <List {...props} sort={{ field: 'id', order: 'DESC' }} perPage={10} pagination={<CoursePagination />} >
+        <Datagrid rowClick="edit" sx={{
+            '& .column-title': { backgroundColor: '#fee' },
+        }}
+        >
+            <TextField source="id" />
+            <TextField source="title" label="코스 타이틀" />
+            <UrlField source="url" label="코스 URL" />
+            <TextField source="companyName" label="회사명" />
+            <TextField source="dates.registrationStartDate" label="등록 시작" />
+            <TextField source="dates.registrationEndDate" label="등록 마감" />
+            <TextField source="dates.courseStartDate" label="코스 시작" />
+            <TextField source="dates.courseEndDate" label="코스 종료" />
+            <TextField source="tags" label="태그" />
+            <NumberField source="cost" label="비용" />
+            <ChipField source="costType" label="비용타입" />
+            <ChipField source="location" label="위치" />
+            <TextField source="onOffline" label="온오프라인" />
+            <ChipField source="prerequisites" label="선수조건" />
+            <BooleanField source="recommended" label="추천" />
+            <BooleanField source="tested" label="코딩테스트" />
+        </Datagrid>
+    </List>
+);
+
+export default CourseList;
