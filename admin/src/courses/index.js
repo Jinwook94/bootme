a62f1b7d654de897129e5fa8929f52cea5221690
Jinwook Kim <kimjinwook94@gmail.com>
2022-12-09 21:57:49 +0900

    feat: 코스 수정 페이지 구현 - CourseEdit

--- admin/src/courses/index.js
+++ admin/src/courses/index.js
@@ -1,7 +1,9 @@
 import CourseList from './CourseList';
 import CourseCreate from './CourseCreate';
+import CourseEdit from './CourseEdit';
 
 export default {
     list: CourseList,
     create: CourseCreate,
+    edit: CourseEdit,
 };
