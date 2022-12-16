f331157fa3b55879ecbd120ee88fce0f48db02d3
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2022-12-16 19:43:12 +0900

    feat: 회사 CRUD 페이지 항목 추가 (#57)
    추가된 항목: 서비스명, 서비스 URL, 로고 이미지 URL

--- admin/src/companies/CompanyEdit.jsx
+++ admin/src/companies/CompanyEdit.jsx
@@ -10,7 +10,10 @@ export const CompanyEdit = () => (
     <Edit>
         <SimpleForm>
             <TextInput source="name" label="회사명" validate={[required()]} />
-            <TextInput source="url" label="회사 URL" validate={[required()]} />
+            <TextInput source="serviceName" label="서비스명" validate={[required()]} />
+            <TextInput source="url" label="회사 URL" validate={[required()]} fullWidth />
+            <TextInput source="serviceUrl" label="서비스 URL" validate={[required()]} fullWidth />
+            <TextInput source="logoUrl" label="로고 이미지 URL" validate={[required()]} fullWidth />
         </SimpleForm>
     </Edit>
 );
