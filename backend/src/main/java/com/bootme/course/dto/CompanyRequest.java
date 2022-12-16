998f8bc7bc1ae52f50edd33fbc6baa665882ab6c
Jinwook Kim <kimjinwook94@gmail.com>
2022-12-16 18:49:34 +0900

    feat: 회사 DTO 필드 추가
    회사 도메인 필드 변경사항 반영

--- backend/src/main/java/com/bootme/course/dto/CompanyRequest.java
+++ backend/src/main/java/com/bootme/course/dto/CompanyRequest.java
@@ -4,24 +4,35 @@ import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Getter
 public class CompanyRequest {
 
-    @NotBlank(message = "회사 url을 입력해주세요.")
-    private String url;
-
     @NotBlank(message = "회사명을 입력해주세요.")
     private String name;
+
+    @NotBlank(message = "회사 서비스명을 입력해주세요.")
+    private String serviceName;
+
+    @NotBlank(message = "회사 URL을 입력해주세요.")
+    private String url;
+
+    @NotBlank(message = "회사 서비스의 URL을 입력해주세요.")
+    private String serviceUrl;
+
+    @NotBlank(message = "회사 로고 이미지 URL을 입력해주세요.")
+    private String logoUrl;
 
     public CompanyRequest(){
     }
 
     @Builder
-    public CompanyRequest(String url, String name) {
-        this.url = url;
+    public CompanyRequest(String name, String serviceName, String url,
+                          String serviceUrl, String logoUrl) {
         this.name = name;
+        this.serviceName = serviceName;
+        this.url = url;
+        this.serviceUrl = serviceUrl;
+        this.logoUrl = logoUrl;
     }
-
 }
--- backend/src/main/java/com/bootme/course/dto/CompanyResponse.java
+++ backend/src/main/java/com/bootme/course/dto/CompanyResponse.java
@@ -12,26 +12,36 @@ import java.util.stream.Collectors;
 public class CompanyResponse {
 
     private Long id;
-    private String url;
     private String name;
+    private String serviceName;
+    private String url;
+    private String serviceUrl;
+    private String logoUrl;
     private List<String> courses;
 
     public CompanyResponse() {
     }
 
     @Builder
-    public CompanyResponse(Long id, String url, String name, List<String> courses) {
+    public CompanyResponse(Long id, String name, String serviceName, String url,
+                           String serviceUrl, String logoUrl, List<String> courses) {
         this.id = id;
-        this.url = url;
         this.name = name;
+        this.serviceName = serviceName;
+        this.url = url;
+        this.serviceUrl = serviceUrl;
+        this.logoUrl = logoUrl;
         this.courses = courses;
     }
 
     public static CompanyResponse of(Company company) {
         return CompanyResponse.builder()
                 .id(company.getId())
-                .url(company.getUrl())
                 .name(company.getName())
+                .serviceName(company.getServiceName())
+                .url(company.getUrl())
+                .serviceUrl(company.getServiceUrl())
+                .logoUrl(company.getLogoUrl())
                 .courses(getCourseTitles(company))
                 .build();
     }
