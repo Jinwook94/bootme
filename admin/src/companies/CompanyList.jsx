f20896689bf37cceb3215fe283dfcb5bf02f0226
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2022-12-16 19:43:12 +0900

    [AD] 회사 CRUD 페이지 항목 추가 (#57)
    추가된 항목: 서비스명, 서비스 URL, 로고 이미지 URL

--- admin/src/companies/CompanyList.jsx
+++ admin/src/companies/CompanyList.jsx
@@ -4,9 +4,7 @@ import {
     TextField,
     UrlField,
     Pagination,
-    ReferenceManyField,
-    SingleFieldList,
-    ChipField
+
 } from 'react-admin';
 
 const CompanyPagination = props => <Pagination rowsPerPageOptions={[10, 25, 50, 100]} {...props} />;
@@ -16,8 +14,11 @@ export const CompanyList = (props) => (
         <Datagrid rowClick="edit"  >
             <TextField source="id" />
             <TextField source="name" label="회사명" />
-            <UrlField source="url" label="회사 URL" />
+            <TextField source="serviceName" label="서비스명" />
             <TextField source="courses" label="개설 코스" />
+            <UrlField source="url" label="회사 URL" />
+            <UrlField source="serviceUrl" label="서비스 URL" />
+            <UrlField source="logoUrl" label="로고 이미지 URL" />
 
             {/* to-do: target 작동하지 않음 -> react-admin 디스코드 채널에 질문*/}
             {/*<ReferenceManyField label="개설 코스" reference="courses" target="companyId" >*/}
