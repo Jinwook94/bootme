961975ff4b66741dc55fa3cdf6ac4394c4d682b6
Jinwook Kim <kimjinwook94@gmail.com>
2022-12-21 11:05:01 +0900

    refactor: msw 관련 코드 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,17 +5,7 @@ import GlobalStyle from './styles/GlobalStyle';
 import App from './App';
 import ReactDOM from 'react-dom';
 
-import { worker } from './mocks/browser';
-
 const rootElement = document.getElementById('root');
-
-// 참고
-// https://mswjs.io/docs/getting-started/integrate/browser#:~:text=Import%20the,the%20examples%20below%3A
-// https://github.com/woowacourse-teams/2022-smody/blob/fd880f2d02c27d185206b121c1eee849a06b5deb/frontend/src/index.tsx
-
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
--- frontend/src/mocks/handlers/index.js
+++
@@ -1,5 +0,0 @@
-import { cardHandlers } from './cardHandlers';
-
-const handlers = [...cardHandlers];
-
-export default handlers;
