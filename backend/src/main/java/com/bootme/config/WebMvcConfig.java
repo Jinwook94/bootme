b4ff9003239b9879b9efb4321cb38971ab3f80da
Jinwook Kim <kimjinwook94@gmail.com>
2022-12-28 12:46:40 +0900

    refactor: CORS 허용 방식 변경 -> WebMvcConfig
    @CrossOrigin 어노테이션을 컨트롤러 클래스마다 붙이면 코드 반복되기 때문에 변경
    WebMvcConfigurer 구현 클래스 WebMvcConfig 작성하여 CORS 허용

---
+++ backend/src/main/java/com/bootme/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.bootme.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,DELETE,TRACE,OPTIONS,PATCH,PUT";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
