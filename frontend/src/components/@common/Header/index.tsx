47528221e04d02022b43cf7debff78d7f1a930df
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2022-12-31 14:32:57 +0900

    [FE] 모바일에서 헤더 아이템들을 메뉴 버튼으로 수정 #83
    모바일 기기에서 헤더에 '부트캠프', '회사', '커뮤니티' 등 헤더 아이템이 노출되면 보기 안좋음

--- frontend/src/components/@common/Header/index.tsx
+++ frontend/src/components/@common/Header/index.tsx
@@ -1,46 +1,84 @@
-import { HeaderLeft, HeaderRight, Wrapper, LogIn, SignIn, HeaderItem, Logo, Dot, ServiceName } from './style';
+import { HeaderLeft, HeaderRight, Wrapper, LogIn, SignIn, HeaderItem, Logo, Dot, ServiceName, Menu } from './style';
 
 import GitHubIcon from '../../../assets/github.svg';
 import { Layout } from '../Layout';
+import { useState } from 'react';
 
 const Header = () => {
   const isLogin = false;
   const username = false;
 
+  const [displaySpan, setDisplaySpan] = useState(1);
+  const handleClick = () => {
+    setDisplaySpan(displaySpan === 1 ? 2 : 1);
+  };
+
   return (
-    <Layout>
-      <Wrapper>
-        <HeaderLeft>
-          <Logo>
-            <GitHubIcon />
-            <ServiceName>BootMe</ServiceName>
-          </Logo>
-          <HeaderItem> 부트캠프 </HeaderItem>
-          <HeaderItem> 회사 </HeaderItem>
-          <HeaderItem> 커뮤니티 </HeaderItem>
-        </HeaderLeft>
-        <HeaderRight>
-          {isLogin && username ? (
-            <>로그인됨</>
-          ) : (
-            <>
-              <LogIn>로그인</LogIn>
-              <Dot
-                width="4"
-                height="4"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="G0E1OVA5O87wEsrH564S"
-                style={{ margin: '0 0.5rem' }}
-              >
-                <circle r="2" transform="matrix(1 0 0 -1 2 2)" fill="#C4C4C4"></circle>
-              </Dot>
-              <SignIn>회원가입</SignIn>
-            </>
-          )}
-        </HeaderRight>
-      </Wrapper>
-    </Layout>
+    <>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0"
+      />
+
+      <Layout>
+        <Wrapper>
+          <HeaderLeft>
+            <Logo>
+              <GitHubIcon />
+              <ServiceName>BootMe</ServiceName>
+            </Logo>
+            <HeaderItem> 부트캠프 </HeaderItem>
+            <HeaderItem> 회사 </HeaderItem>
+            <HeaderItem> 커뮤니티 </HeaderItem>
+          </HeaderLeft>
+          <HeaderRight>
+            {isLogin && username ? (
+              <>로그인됨</>
+            ) : (
+              <>
+                <LogIn>로그인</LogIn>
+                <Dot
+                  width="4"
+                  height="4"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="G0E1OVA5O87wEsrH564S"
+                  style={{ margin: '0 0.5rem' }}
+                >
+                  <circle r="2" transform="matrix(1 0 0 -1 2 2)" fill="#C4C4C4"></circle>
+                </Dot>
+                <SignIn>회원가입</SignIn>
+                <Menu onClick={handleClick}>
+                  {displaySpan === 1 ? (
+                    <span
+                      className="material-symbols-outlined"
+                      style={{
+                        fontSize: '1.5rem',
+                        verticalAlign: 'sub',
+                        marginLeft: '1rem',
+                      }}
+                    >
+                      menu
+                    </span>
+                  ) : (
+                    <span
+                      className="material-symbols-outlined"
+                      style={{
+                        fontSize: '1.5rem',
+                        verticalAlign: 'sub',
+                        marginLeft: '1rem',
+                      }}
+                    >
+                      close
+                    </span>
+                  )}
+                </Menu>
+              </>
+            )}
+          </HeaderRight>
+        </Wrapper>
+      </Layout>
+    </>
   );
 };
 
