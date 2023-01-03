fe2f06f48ee6507f56ebbe8878c49ebbbfa67bbd
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2023-01-03 17:47:38 +0900

    [FE] 코스 조회 필터링 기능 구현 (사이드 필터) (#89)
    * feat: 필터링 관련 state 관리 위해 useFilters 커스텀 훅 구현
    
    - addFilter(filter) : 필터 추가
    - removeBeforeAdd(label, currentValue)
        - Range, Text Input 에서 필터를 추가하기 전에 해당 필터 값 보다 높은 값을 가진 필터를 삭제
        - 예를 들어 ‘600만원 이하’ 값을 입력하거나 선택했을 때, 이전에 입력된 필터 값이 ‘800만원 이하’라면 600~800만원 값을 가진 카드도 포함됨
        - 그러므로 Range or Text input 에서 필터를 추가할 때는 입력된 필터 값보다는 높은 값을 가진 필터를 삭제한 후 필터를 추가해야함
        - Checkbox input은 값을 사용하지 않으므로 이 메서드를 사용하지 않음
    - removeFilter(filter) : 필터 삭제
    
    * feat: 체크박스 필터링 구현
    
    - unchecked 상태에서 체크박스 클릭 → 필터 추가
    - checked 상태에서 체크박스 클릭 → 필터 삭제
    
    * feat: Range bar 필터링 구현
    
    * feat: Text Input 필터링 구현
    
    * feat: Home 페이지에 필터링 적용

--- frontend/src/components/SideFilter/FilterOption/index.tsx
+++ frontend/src/components/SideFilter/FilterOption/index.tsx
@@ -1,14 +1,17 @@
 import { BoxWrapper, Checkbox, ListItem, Option, Wrapper } from './style';
 import React, { useState } from 'react';
+import { useFilters } from '../../../hooks/useFilters';
 
 export const FilterOption = ({ filterOption, borderTop }: FilterOptionProps) => {
   const [isChecked, setIsChecked] = useState(false);
+  const { addFilter, removeFilter } = useFilters();
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setIsChecked(event.target.checked);
   }
   function handleListItemClick() {
     setIsChecked(!isChecked);
+    isChecked ? removeFilter(filterOption) : addFilter(filterOption);
   }
 
   return (
---
+++ frontend/src/hooks/useFilters.tsx
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const FilterContext = createContext<FilterContextProps>({
+  selectedFilters: [],
+  addFilter: () => {},
+  removeBeforeAdd: () => {},
+  removeFilter: () => {},
+});
+
+export const FilterProvider = ({ children }: FilterProviderProps) => {
+  const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
+
+  const addFilter = (filter: string) => {
+    setSelectedFilters([...selectedFilters, filter]);
+  };
+
+  const removeBeforeAdd = (label: string, currentValue: number) => {
+    const newSelectedFilters = selectedFilters.filter(
+      filter => filter.startsWith(label) && Number(filter.split('-')[1]) >= currentValue
+    );
+    for (const filter of newSelectedFilters) {
+      removeFilter(filter);
+    }
+  };
+  const removeFilter = (filter: string) => {
+    const index = selectedFilters.indexOf(filter);
+    if (index !== -1) {
+      selectedFilters.splice(index, 1);
+    }
+    setSelectedFilters(selectedFilters.filter(f => f !== filter));
+  };
+
+  return (
+    <FilterContext.Provider value={{ selectedFilters, addFilter, removeBeforeAdd, removeFilter }}>
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+export const useFilters = () => useContext(FilterContext);
+
+interface FilterContextProps {
+  selectedFilters: string[];
+  removeBeforeAdd: (label: string, currentValue: number) => void;
+  addFilter: (filter: string) => void;
+  removeFilter: (filter: string) => void;
+}
+
+interface FilterProviderProps {
+  children: React.ReactNode;
+}
