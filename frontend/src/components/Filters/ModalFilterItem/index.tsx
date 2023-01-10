9e417649d70e90aa6c3a2a541185df95c40adda2
Jinwook Kim <kimjinwook94@gmail.com>
2023-01-10 15:37:18 +0900

    feat: 필터 그룹별로 다른 UI 보여지도록 구현
    - 개발 분야, 기술 스택, 코딩 테스트: 체크박스만
    - 비용: 체크박스 + Range
    - 수강 기간: Range

--- frontend/src/components/Filters/ModalFilterItem/index.tsx
+++ frontend/src/components/Filters/ModalFilterItem/index.tsx
@@ -1,9 +1,18 @@
 import FilterOption from '../FilterOption';
-import { CourseFilterTypes, MODAL_FILTER } from '../../../constants/courseFilter';
+import {
+  CATEGORIES,
+  COST_TYPE,
+  CourseFilterTypes,
+  MODAL_FILTER,
+  PERIOD,
+  STACKS,
+  TEST,
+} from '../../../constants/courseFilter';
 import React, { useRef, useState } from 'react';
 import { Wrapper, Title, MoreButton, MoreOptions } from './style';
+import RangeBar from '../RangeBar';
 
-const ModalFilterItem = ({ filterName, filterOptions, isMore, isReset }: ModalFilterItemProps) => {
+const ModalFilterItem = ({ filter, filterName, filterOptions, isMore, isReset }: ModalFilterItemProps) => {
   const [isMoreOpen, setIsMoreOpen] = useState(false);
   const wrapperRef = useRef<HTMLDivElement>(null);
 
@@ -17,44 +26,134 @@ const ModalFilterItem = ({ filterName, filterOptions, isMore, isReset }: ModalFi
     }
   };
 
-  return (
-    <Wrapper ref={wrapperRef}>
-      <Title>{filterName}</Title>
-      {filterOptions.slice(0, 5).map((filterOption: string, index) => (
-        <FilterOption
-          key={index}
-          filterType={MODAL_FILTER}
-          filterName={filterName}
-          filterOption={filterOption}
-          isReset={isReset}
-        />
-      ))}
-      {isMore && !isMoreOpen ? (
-        <MoreButton onClick={handleMoreClick}>
-          <span>더 표시</span>
-        </MoreButton>
-      ) : null}
-      <MoreOptions isMoreOpen={isMoreOpen}>
-        {isMoreOpen &&
-          filterOptions
-            .slice(5)
-            .map((filterOption: string, index) => (
-              <FilterOption
-                key={index}
-                filterType={MODAL_FILTER}
-                filterName={filterName}
-                filterOption={filterOption}
-                isReset={isReset}
-              />
-            ))}
-        {isMore && isMoreOpen ? (
-          <MoreButton onClick={handleMoreClick}>
-            <span onClick={handleHideClick}>숨기기</span>
-          </MoreButton>
-        ) : null}
-      </MoreOptions>
-    </Wrapper>
-  );
+  switch (filter) {
+    case CATEGORIES:
+      return (
+        <Wrapper ref={wrapperRef}>
+          <Title>{filterName}</Title>
+          {filterOptions.slice(0, 3).map((filterOption: string, index) => (
+            <FilterOption
+              key={index}
+              filterType={MODAL_FILTER}
+              filterName={filterName}
+              filterOption={filterOption}
+              isReset={isReset}
+            />
+          ))}
+          {isMore && !isMoreOpen ? (
+            <MoreButton onClick={handleMoreClick}>
+              <span>더 표시</span>
+            </MoreButton>
+          ) : null}
+          <MoreOptions isMoreOpen={isMoreOpen}>
+            {isMoreOpen &&
+              filterOptions
+                .slice(3)
+                .map((filterOption: string, index) => (
+                  <FilterOption
+                    key={index}
+                    filterType={MODAL_FILTER}
+                    filterName={filterName}
+                    filterOption={filterOption}
+                    isReset={isReset}
+                    borderTop={index === 9}
+                  />
+                ))}
+            {isMore && isMoreOpen ? (
+              <MoreButton onClick={handleMoreClick}>
+                <span onClick={handleHideClick}>숨기기</span>
+              </MoreButton>
+            ) : null}
+          </MoreOptions>
+        </Wrapper>
+      );
+    case STACKS:
+      return (
+        <Wrapper ref={wrapperRef}>
+          <Title>{filterName}</Title>
+          {filterOptions.slice(0, 3).map((filterOption: string, index) => (
+            <FilterOption
+              key={index}
+              filterType={MODAL_FILTER}
+              filterName={filterName}
+              filterOption={filterOption}
+              isReset={isReset}
+              borderTop={index === 8}
+            />
+          ))}
+          {isMore && !isMoreOpen ? (
+            <MoreButton onClick={handleMoreClick}>
+              <span>더 표시</span>
+            </MoreButton>
+          ) : null}
+          <MoreOptions isMoreOpen={isMoreOpen}>
+            {isMoreOpen &&
+              filterOptions
+                .slice(3)
+                .map((filterOption: string, index) => (
+                  <FilterOption
+                    key={index}
+                    filterType={MODAL_FILTER}
+                    filterName={filterName}
+                    filterOption={filterOption}
+                    isReset={isReset}
+                  />
+                ))}
+            {isMore && isMoreOpen ? (
+              <MoreButton onClick={handleMoreClick}>
+                <span onClick={handleHideClick}>숨기기</span>
+              </MoreButton>
+            ) : null}
+          </MoreOptions>
+        </Wrapper>
+      );
+    case COST_TYPE:
+      return (
+        <Wrapper ref={wrapperRef}>
+          <Title>{filterName}</Title>
+          <FilterOption
+            filterType={MODAL_FILTER}
+            filterName={filterName}
+            filterOption={filterOptions[0]}
+            isReset={isReset}
+          />
+          <FilterOption
+            filterType={MODAL_FILTER}
+            filterName={filterName}
+            filterOption={filterOptions[1]}
+            isReset={isReset}
+          />
+          <RangeBar filterName={filterName} isReset={isReset} />
+        </Wrapper>
+      );
+    case PERIOD:
+      return (
+        <Wrapper ref={wrapperRef}>
+          <Title>{filterName}</Title>
+          <RangeBar filterName={filterName} isReset={isReset} />
+        </Wrapper>
+      );
+    case TEST:
+      return (
+        <Wrapper ref={wrapperRef}>
+          <Title>{filterName}</Title>
+          <FilterOption
+            filterType={MODAL_FILTER}
+            filterName={filterName}
+            filterOption={filterOptions[0]}
+            isReset={isReset}
+          />
+          <FilterOption
+            filterType={MODAL_FILTER}
+            filterName={filterName}
+            filterOption={filterOptions[1]}
+            isReset={isReset}
+          />
+        </Wrapper>
+      );
+    default:
+      return null;
+  }
 };
 
 export type ModalFilterItemProps = CourseFilterTypes & {
