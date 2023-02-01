80a7ad663d6fbe49484808a7b4961f20a498bd81
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-01 23:56:14 +0900

    feat: 구글 One Tap 로그인 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,21 @@ import Home from './pages/Home';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { FilterProvider } from './hooks/useFilters';
 import { LoginProvider } from './hooks/useLogin';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <LoginProvider>
-      <FilterProvider>
-        <QueryClientProvider client={queryClient}>
-          <Home />
-        </QueryClientProvider>
-      </FilterProvider>
-    </LoginProvider>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID as string}>
+      <LoginProvider>
+        <FilterProvider>
+          <QueryClientProvider client={queryClient}>
+            <Home />
+          </QueryClientProvider>
+        </FilterProvider>
+      </LoginProvider>
+    </GoogleOAuthProvider>
   );
 };
 
