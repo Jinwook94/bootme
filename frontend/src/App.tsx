845b0460763eebf275226ccaca384ad341b57021
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-13 20:52:28 +0900

    [FE] Provider들을 App.tsx에서 index.tsx로 이동

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
 import Home from './pages/Home';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { FilterProvider } from './hooks/useFilters';
-import { LoginProvider } from './hooks/useLogin';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
-const queryClient = new QueryClient();
 
 const App = () => {
-  return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID as string}>
-      <LoginProvider>
-        <FilterProvider>
-          <QueryClientProvider client={queryClient}>
-            <Home />
-          </QueryClientProvider>
-        </FilterProvider>
-      </LoginProvider>
-    </GoogleOAuthProvider>
-  );
+  return <Home />;
 };
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,29 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import GlobalStyle from './styles/GlobalStyle';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { FilterProvider } from './hooks/useFilters';
+import { LoginProvider } from './hooks/useLogin';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 import ReactDOM from 'react-dom/client';
 
 const rootElement = document.getElementById('root') as Element;
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(rootElement).render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
-  </ThemeProvider>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID as string}>
+    <LoginProvider>
+      <FilterProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <App />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </FilterProvider>
+    </LoginProvider>
+  </GoogleOAuthProvider>
 );
