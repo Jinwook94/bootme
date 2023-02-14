6fa2377137e65488f038f8b9afbd22387e43bc37
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-14 20:19:43 +0900

    feat: 카카오 로그인 완료 후 이전 페이지로 리다이렉트 구현
    - 카카오 로그인 과정
     1. KakaoLoginButton 클릭: 카카오 인증 서버로 Access Token 받기를 요청하면, 카카오 인증 서버가 사용자에게 카카오계정 로그인을 통한 인증을 요청
     2. 사용자가 카카오 계정 로그인 완료하면 리디렉션된 URI 에서 Access Token 을 추출
     3. sendAccessTokenToKakao(): Access Token 등 인증 정보를 포함하여 kauth 서버로 POST 요청을 보내고 응답으로 ID Token 수신
     4. sendIdTokenToServer(): 수신한 ID Token 을 BootMe 서버로 전송
     5. 로컬스토리지 Login 값을 true 로 변경하고 이전 페이지로 리다이렉트
    
    - 카카오 로그인 REST API Docs 참고
      - https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api

--- frontend/src/components/LoginModal/KakaoLogin/index.tsx
+++ frontend/src/components/LoginModal/KakaoLogin/index.tsx
@@ -1,20 +1,28 @@
 import { useEffect } from 'react';
-import axios from 'axios';
 import qs from 'qs';
 import LoginButton from '../LoginButton';
 import { useLogin } from '../../../hooks/useLogin';
+import { useNavigate } from 'react-router-dom';
+import { noCredentialsFetcher } from '../../../api/fetcher';
 
-const KakaoLogin = () => {
-  const { sendIdTokenToServer } = useLogin();
-  const REST_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
-  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
-  const STATE = process.env.REACT_KAKAO_STATE;
-  const CLIENT_SECRET = process.env.REACT_KAKAO_CLIENT_SECRET;
+const REST_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
+const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI_KAKAO;
+const STATE = process.env.REACT_KAKAO_STATE;
+const KAKAO_OAUTH_ACCESS_TOKEN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&state=${STATE}`;
+const KAKAO_OAUTH_ID_TOKEN_URL = 'https://kauth.kakao.com/oauth/token';
+const CLIENT_SECRET = process.env.REACT_APP_KAKAO_CLIENT_SECRET;
 
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&state=${STATE}`;
-  const code = new URL(window.location.href).searchParams.get('code');
+export const KakaoLoginButton = () => {
+  return (
+    <div onClick={() => (window.location.href = KAKAO_OAUTH_ACCESS_TOKEN_URL)}>
+      <LoginButton client={'kakao'} />
+    </div>
+  );
+};
 
-  const getToken = async () => {
+export const KakaoLogin = () => {
+  const sendAccessTokenToKakao = async () => {
+    const code = new URL(window.location.href).searchParams.get('code');
     const payload = qs.stringify({
       grant_type: 'authorization_code',
       client_id: REST_API_KEY,
@@ -22,52 +30,40 @@ const KakaoLogin = () => {
       code: code,
       client_secret: CLIENT_SECRET,
     });
-    try {
-      const res = await axios.post('https://kauth.kakao.com/oauth/token', payload);
 
-      window.Kakao.init(REST_API_KEY);
-      window.Kakao.Auth.setAccessToken(res.data.access_token);
-      const data = await window.Kakao.API.request({
-        url: '/v2/user/me',
+    return noCredentialsFetcher
+      .post(KAKAO_OAUTH_ID_TOKEN_URL, payload, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+      })
+      .then(r => {
+        window.Kakao.init(REST_API_KEY);
+        window.Kakao.Auth.setAccessToken(r.data.access_token);
+        return r.data.id_token;
+      })
+      .catch(error => {
+        console.log(error);
       });
-
-      console.log(
-        '< Kakao Login > \n\nAuthorization Code: ' +
-          code +
-          '\n\nAccess Token: ' +
-          res.data.access_token +
-          '\n\nUser ID: ' +
-          data.id +
-          '\n\nUser Nickname: ' +
-          data.properties.nickname
-      );
-      console.log('---- 카카오 로그인 응답 ----');
-      console.log(res);
-      console.log('-----------------------');
-      const idToken = res.data.id_token;
-      sendIdTokenToServer(idToken);
-    } catch (err) {
-      console.log(err);
-    }
   };
+  const { sendIdTokenToServer } = useLogin();
+  const navigate = useNavigate();
 
+  /**
+   *  1. KakaoLoginButton: 카카오 인증 서버로 Access Token 받기를 요청하면, 카카오 인증 서버가 사용자에게 카카오계정 로그인을 통한 인증을 요청
+   *  2. 사용자가 카카오 계정 로그인 완료하면 리디렉션된 URI 에서 Access Token 을 추출
+   *  3. sendAccessTokenToKakao(): Access Token 등 인증 정보를 포함하여 kauth 서버로 POST 요청을 보내고 응답으로 ID Token 수신
+   *  4. sendIdTokenToServer(): 수신한 ID Token 을 BootMe 서버로 전송
+   *  5. 로컬스토리지 Login 값을 true 로 변경하고 이전 페이지로 리다이렉트
+   * */
   useEffect(() => {
-    if (code) {
-      getToken();
-    }
+    sendAccessTokenToKakao().then(idToken =>
+      sendIdTokenToServer(idToken).then(() => {
+        localStorage.setItem('Login', 'true');
+        navigate(-1);
+      })
+    );
   }, []);
 
-  const handleClick = () => {
-    window.location.href = KAKAO_AUTH_URL;
-  };
-
-  return (
-    <>
-      <div onClick={handleClick}>
-        <LoginButton client={'kakao'} />
-      </div>
-    </>
-  );
+  return <></>;
 };
-
-export default KakaoLogin;
