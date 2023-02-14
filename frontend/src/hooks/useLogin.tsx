f3a0d3c55bf070a8704bb61a31cc2aa560823d13
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-14 20:14:34 +0900

    feat: sendIdTokenToServer()가 Promise 반환하도록 변경
    - 카카오 로그인 '토큰 받기' 처리를 구현할 때, sendIdTokenToServer()를 사용할 예정임
    - sendIdTokenToServer() 호출 성공 이후 '이전 페이지로 이동' 등 작업을 이어서 처리하기 위해 Promise를 반환하도록 변경함

--- frontend/src/hooks/useLogin.tsx
+++ frontend/src/hooks/useLogin.tsx
@@ -4,7 +4,7 @@ import { fetcher } from '../api/fetcher';
 const LoginContext = createContext<LoginContextProps>({
   isLoginModal: false,
   handleLoginModal: () => {},
-  sendIdTokenToServer: () => {},
+  sendIdTokenToServer: () => Promise.resolve(),
   handleLogOut: () => {},
 });
 
@@ -16,7 +16,7 @@ export const LoginProvider = ({ children }: LoginProviderProps) => {
   };
 
   const sendIdTokenToServer = (idToken: string | undefined) => {
-    fetcher
+    return fetcher
       .post('/login', null, {
         headers: {
           Authorization: 'Bearer ' + idToken,
@@ -38,6 +38,7 @@ export const LoginProvider = ({ children }: LoginProviderProps) => {
     fetcher
       .post('/logout', null, {})
       .then(() => {
+        localStorage.setItem('Login', 'false');
         localStorage.removeItem('NickName');
         localStorage.removeItem('ProfileImage');
         location.reload();
@@ -66,7 +67,7 @@ export const useLogin = () => useContext(LoginContext);
 interface LoginContextProps {
   isLoginModal: boolean;
   handleLoginModal: () => void;
-  sendIdTokenToServer: (idToken: string | undefined) => void;
+  sendIdTokenToServer: (idToken: string | undefined) => Promise<void>;
   handleLogOut: () => void;
 }
 
