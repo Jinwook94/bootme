d043393a4a31ccee5ae48525bcb8e6b097d1ac2f
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-16 21:40:40 +0900

    [BE] JPA 관련 설정 분리
    BootmeApplication 클래스에 @EnableJpaAuditing 붙이면 테스트 코드 에러 발생하여 분리

---
+++ backend/src/main/java/com/bootme/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.bootme.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
