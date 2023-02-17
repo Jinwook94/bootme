6a87941801f2674fda257e82761447dafcc457b6
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-17 15:43:15 +0900

    [BE] JwtVo.Header, Body 기본 생성자 추가 및 필드에서 final 제거
    - 기본 생성자 없고, 상수 필드인 경우 ObjectMapper 사용한 객체 복사시 에러 생겨서 생성자 추가 및 final 제거
    
    - AuthService.copyTokenToVo()에서 에러 발생

--- backend/src/main/java/com/bootme/auth/dto/JwtVo.java
+++ backend/src/main/java/com/bootme/auth/dto/JwtVo.java
@@ -14,9 +14,12 @@ public class JwtVo {
 
     @Getter
     public static class Header{
-        private final String kid;
-        private final String typ;
-        private final String alg;
+        private String kid;
+        private String typ;
+        private String alg;
+
+        public Header() {
+        }
 
         @Builder
         public Header(String kid, String typ, String alg) {
@@ -29,29 +32,32 @@ public class JwtVo {
     @Getter
     public static class Body{
         private String OAuthProvider;
-        private final String iss;
-        private final String aud;
-        private final String sub;
-        private final Long iat;
-        private final Long exp;
-        private final String email;
+        private String iss;
+        private String aud;
+        private String sub;
+        private Long iat;
+        private Long exp;
+        private String email;
 
-        private final String name;
-        private final String picture;
-        private final String given_name;
-        private final String family_name;
-        private final String azp;
-        private final String jti;
-        private final Long nbf;
-        private final boolean email_verified;
-        private final String ageRange;
-        private final String birthDay;
-        private final String birthYear;
-        private final String gender;
-        private final String id;
-        private final String phoneNumber;
-        private final String nickname;
-        private final String auth_time;
+        private String name;
+        private String picture;
+        private String given_name;
+        private String family_name;
+        private String azp;
+        private String jti;
+        private Long nbf;
+        private boolean email_verified;
+        private String ageRange;
+        private String birthDay;
+        private String birthYear;
+        private String gender;
+        private String id;
+        private String phoneNumber;
+        private String nickname;
+        private String auth_time;
+
+        public Body() {
+        }
 
         @Builder
         public Body(String OAuthProvider, String iss, String aud, String sub, Long iat, Long exp, String email, String name, String picture, String given_name, String family_name, String azp, String jti, Long nbf, boolean email_verified, String ageRange, String birthDay, String birthYear, String gender, String id, String phoneNumber, String nickname, String auth_time) {
