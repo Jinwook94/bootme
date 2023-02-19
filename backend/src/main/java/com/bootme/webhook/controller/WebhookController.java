9689ae90cfccf9c9ce0cc1c50891fccea270dd3c
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-20 02:26:07 +0900

    feat: 웹훅 엔드포인트 구현
    - 헤더로 전달된 토큰 검증 후 이벤트에 맞는 처리 진행

---
+++ backend/src/main/java/com/bootme/webhook/controller/WebhookController.java
@@ -0,0 +1,62 @@
+package com.bootme.webhook.controller;
+
+import com.bootme.auth.service.AuthService;
+import com.bootme.course.service.CourseService;
+import com.bootme.webhook.dto.WebhookRequest;
+import com.bootme.webhook.exception.InvalidEventException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+import static com.bootme.common.exception.ErrorType.INVALID_EVENT;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/webhook")
+public class WebhookController {
+
+    private final AuthService authService;
+    private final CourseService courseService;
+
+    private final String WEBHOOK_SECRET;
+
+    public WebhookController(AuthService authService,
+                             CourseService courseService,
+                             @Value("${security.jwt.bootme.webhook-secret}") String WEBHOOK_SECRET) {
+        this.authService = authService;
+        this.courseService = courseService;
+        this.WEBHOOK_SECRET = WEBHOOK_SECRET;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> handleWebhook(@RequestHeader(name = "Bootme_Secret") String secret,
+                                           @RequestBody WebhookRequest webhookRequest) {
+        final String COURSE_CLICKED = "courseClicked";
+        final String COURSE_BOOKMARKED = "courseBookmarked";
+        final String EVENT = webhookRequest.getEvent();
+        final Map<String, String> DATA = webhookRequest.getData();
+        final Long COURSE_ID = Long.parseLong(DATA.get("courseId"));
+
+        authService.verifyWebhookJwt(secret, WEBHOOK_SECRET);
+
+        try {
+            switch (EVENT) {
+                case COURSE_CLICKED:
+                    courseService.incrementClicks(COURSE_ID);
+                    break;
+                case COURSE_BOOKMARKED:
+                    courseService.incrementBookmarks(COURSE_ID);
+                    break;
+                default:
+                    throw new InvalidEventException(INVALID_EVENT, webhookRequest.getEvent());
+            }
+        } catch (InvalidEventException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
