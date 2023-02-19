259d2479015f797702e7821a5e4a6276487b7f47
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-20 02:24:35 +0900

    feat: 코스의 클릭수, 북마크수 증가 시키는 메서드 구현

--- backend/src/main/java/com/bootme/course/repository/CourseRepository.java
+++ backend/src/main/java/com/bootme/course/repository/CourseRepository.java
@@ -2,8 +2,23 @@ package com.bootme.course.repository;
 
 import com.bootme.course.domain.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Course c SET c.clicks = c.clicks + 1 WHERE c.id = :id")
+    void incrementClicks(@Param("id") Long id);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Course c SET c.bookmarks = c.bookmarks + 1 WHERE c.id = :id")
+    void incrementBookmarks(@Param("id") Long id);
+
 }
