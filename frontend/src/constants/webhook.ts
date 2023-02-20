7d097f396503877c2784bfc3cbcddebd0d14dd31
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-20 17:17:14 +0900

    feat: useWebhook 구현
    sendWebhookNoti()
    - 지정된 이벤트 발생시 서버로 웹훅 요청을 보내는 함수
    - 웹훅 요청 검증 목적으로 헤더에 JWT 첨부

---
+++ frontend/src/constants/webhook.ts
@@ -0,0 +1,10 @@
+/**
+ * Events
+ * */
+export const COURSE_CLICKED = 'courseClicked';
+export const COURSE_BOOKMARKED = 'courseBookmarked';
+
+/**
+ * Data
+ * */
+export const COURSE_ID = 'courseId';
