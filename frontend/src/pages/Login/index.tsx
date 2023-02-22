3d1e0051bb167ea93fa135dc1f47c9ca9ef36b91
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-23 03:17:25 +0900

    feat: 로그인 페이지 구현
    - 로그인 되지 않은 유저가 Private Route 인 북마크 관리 페이지에 접근할 경우 로그인 페이지로 리다이렉트하기 위해 구현함

---
+++ frontend/src/pages/Login/index.tsx
@@ -0,0 +1,39 @@
+import {
+  LoginOptions,
+  TermsOfService,
+  WelcomeText,
+  Wrapper1,
+  Wrapper2,
+  Wrapper3,
+  Wrapper4,
+  Wrapper5,
+} from '../../components/LoginModal/style';
+import { GoogleLoginButton } from '../../components/LoginModal/GoogleLogin';
+import { NaverLoginButton } from '../../components/LoginModal/NaverLogin';
+import { KakaoLoginButton } from '../../components/LoginModal/KakaoLogin';
+import React from 'react';
+
+const Login = () => {
+  return (
+    // todo: LoginModal 컴포넌트와 동일한 코드이므로 아래 코드를 따로 컴포넌트 분리 필요
+    <Wrapper1>
+      <Wrapper2>
+        <Wrapper3>
+          <Wrapper4>
+            <Wrapper5>
+              <WelcomeText>부트미에 오신것을 환영합니다. </WelcomeText>
+              <LoginOptions>
+                <GoogleLoginButton />
+                <NaverLoginButton />
+                <KakaoLoginButton />
+              </LoginOptions>
+              <TermsOfService>약관</TermsOfService>
+            </Wrapper5>
+          </Wrapper4>
+        </Wrapper3>
+      </Wrapper2>
+    </Wrapper1>
+  );
+};
+
+export default Login;
