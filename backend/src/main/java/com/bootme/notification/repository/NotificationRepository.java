69b14c211a3b86b9aedfdde593269f9e7b523f9f
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-25 22:08:51 +0900

    feat: NotificationRepository 구현

---
+++ backend/src/main/java/com/bootme/notification/repository/NotificationRepository.java
@@ -0,0 +1,22 @@
+package com.bootme.notification.repository;
+
+import com.bootme.notification.domain.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    @Query("SELECT n FROM Notification n WHERE n.member.id = :memberId")
+    List<Notification> findByMemberId(@Param("memberId") Long memberId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Notification n SET n.checked = true WHERE n.member.id = :memberId")
+    void markAllAsCheckedForMember(@Param("memberId") Long memberId);
+
+}
