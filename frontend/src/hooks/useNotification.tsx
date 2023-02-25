66e4b1f4e9fb09b2c7d0bac100fb7aa8de8bc905
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-25 15:24:12 +0900

    feat: 알림 확인 구현
    - 확인되지 않은 알림 있으면 NotificationActiveIcon 표시됨
    - 알림 버튼 클릭시 모든 알림 확인된 것으로 변경됨

--- frontend/src/hooks/useNotification.tsx
+++ frontend/src/hooks/useNotification.tsx
@@ -1,21 +1,57 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import { fetcher } from '../api/fetcher';
 
 const NotificationContext = createContext<NotificationContextProps>({
   notifications: [],
   setNotifications: () => {},
+  isAllChecked: true,
+  setIsAllChecked: () => {},
+  getNotifications: () => {},
+  updateNotifications: () => Promise.resolve(),
 });
 
 export const NotificationProvider = ({ children }: NotificationProviderProps) => {
   const [notifications, setNotifications] = useState<Notification[]>([]);
+  const [isAllChecked, setIsAllChecked] = useState<boolean>(true);
+
   useEffect(() => {
-    setNotifications(JSON.parse(localStorage.getItem('Notifications') || '[]'));
-  }, [localStorage.getItem('Notifications')]);
+    // 알림 중 checked 값이 false 인 것이 하나라도 있으면 => isAllChecked = false
+    const isAllChecked = !notifications.some(notification => !notification.checked);
+    setIsAllChecked(isAllChecked);
+  }, [notifications]);
+
+  const getNotifications = (memberId: number) => {
+    fetcher
+      .get(`/notifications/${memberId}`)
+      .then(r => {
+        setNotifications(r.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const updateNotifications = (memberId: number) => {
+    return fetcher
+      .put(`/notifications/${memberId}/checked`)
+      .then(r => {
+        setNotifications(r.data);
+        setIsAllChecked(true);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   return (
     <NotificationContext.Provider
       value={{
         notifications,
         setNotifications,
+        isAllChecked,
+        setIsAllChecked,
+        getNotifications,
+        updateNotifications,
       }}
     >
       {children}
@@ -28,6 +64,10 @@ export const useNotification = () => useContext(NotificationContext);
 interface NotificationContextProps {
   notifications: Notification[];
   setNotifications: React.Dispatch<Notification[]>;
+  isAllChecked: boolean;
+  setIsAllChecked: React.Dispatch<boolean>;
+  getNotifications: (memberId: number) => void;
+  updateNotifications: (memberId: number) => Promise<void>;
 }
 
 interface NotificationProviderProps {
@@ -41,5 +81,6 @@ interface Notification {
   memberId: number;
   courseId: number;
   courseTitle: string;
+  checked: boolean;
   createdAt: number;
 }
