f062e7994ee11f6581c041eb614b3694b2bc7bdf
Jinwook Kim <kimjinwook94@gmail.com>
2023-02-25 22:09:08 +0900

    feat: NotificationService 구현

---
+++ backend/src/main/java/com/bootme/notification/service/NotificationService.java
@@ -0,0 +1,36 @@
+package com.bootme.notification.service;
+
+import com.bootme.member.domain.Member;
+import com.bootme.notification.domain.Notification;
+import com.bootme.notification.dto.NotificationResponse;
+import com.bootme.notification.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+    @Transactional(readOnly = true)
+    public List<NotificationResponse> findByMemberId(Long memberId){
+        List<Notification> notificationList = notificationRepository.findByMemberId(memberId);
+        return notificationList.stream().map(NotificationResponse::of).collect(Collectors.toList());
+    }
+
+    public Long sendNotification(Member member, String event){
+        Notification notification = Notification.of(member, event);
+        return notificationRepository.save(notification).getId();
+    }
+
+    public void markAllAsCheckedForMember(Long memberId) {
+        notificationRepository.markAllAsCheckedForMember(memberId);
+    }
+
+}
