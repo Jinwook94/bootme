bbb297b9d062626b910d0f2e26368cc280fcad1a
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2023-02-28 14:56:39 +0900

    [BE] GlobalExceptionHandler 개선 (#197)
    * feat: 각 ExceptionHandler 메서드 리턴문에서 HttpStatus 표시되도록 변경
    
    - 기존에는 리턴문에서 handleExceptionInternal() 호출하며 각 ExceptionHandler 메서드가 어떤 HttpStatus 를 반환하는지 알기 어려웠음
    - 각 ExceptionHandler 메서드 return statement 에서 어떤 HttpStatus 반환되는지 바로 알 수 있도록 수정함
    - ExceptionHandler 메서드에서 HttpStatus 입력하기 때문에 ErrorType 에서는 더 이상 HttpStatus 필요 없어져서 삭제함
    
    * feat: GlobalExceptionHandler 에서 ErrorResponse 팩토리 메서드 제거
    
    GlobalExceptionHandler 클래스에 구현되어 있던 ErrorResponse 팩토리 메서드를 삭제하고 ErrorResponse 클래스에 구현함
    
    GlobalExceptionHandler 클래스 코드 줄이기 위한 목적
    
    * feat: 런타임 예외 핸들러 메서드 추가

--- backend/src/main/java/com/bootme/common/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/bootme/common/exception/handler/GlobalExceptionHandler.java
@@ -6,10 +6,13 @@ import com.bootme.common.exception.ForbiddenException;
 import com.bootme.common.exception.dto.ErrorResponse;
 import com.bootme.common.exception.ErrorType;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static com.bootme.common.exception.ErrorType.RUNTIME_EXCEPTION;
 
 @RestControllerAdvice
 @Slf4j
@@ -19,32 +22,27 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ErrorResponse> badRequestExceptionHandler(final BadRequestException e) {
         log.warn("Bad Request Exception", e);
         ErrorType errorType = e.getErrorType();
-        return handleExceptionInternal(errorType);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(errorType));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorResponse> unauthorizedExceptionHandler(final UnauthorizedException e) {
         log.warn("Unauthorized Exception", e);
         ErrorType errorType = e.getErrorType();
-        return handleExceptionInternal(errorType);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.of(errorType));
     }
 
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<ErrorResponse> forbiddenExceptionHandler(final ForbiddenException e) {
         log.warn("Forbidden Exception", e);
         ErrorType errorType = e.getErrorType();
-        return handleExceptionInternal(errorType);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ErrorResponse.of(errorType));
     }
 
-    private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorType errorType) {
-        return ResponseEntity.status(errorType.getHttpStatus()).body(makeErrorResponse(errorType));
-    }
-
-    private ErrorResponse makeErrorResponse(ErrorType errorType) {
-        return ErrorResponse.builder().
-                errorCode(errorType.getErrorCode()).
-                message(errorType.getMessage()).
-                build();
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        log.warn("Runtime Exception", e);
+        return ResponseEntity.internalServerError().body(ErrorResponse.of(RUNTIME_EXCEPTION));
     }
 
 }
