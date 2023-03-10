c8cdd5824805388c9602a8ce5d46eaf7315d11d0
Jinwook Kim <kimjinwook94@gmail.com>
2023-03-11 02:01:05 +0900

    [BE] CORS 허용 URL에 bootme.co.kr 추가

--- backend/src/main/java/com/bootme/config/WebMvcConfig.java
+++ backend/src/main/java/com/bootme/config/WebMvcConfig.java
@@ -12,7 +12,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                .allowedOrigins("http://localhost:3000",
+                                "http://localhost:3001",
+                                "https://bootme.co.kr",
+                                "https://www.bootme.co.kr")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowedHeaders("*")
                 .allowCredentials(true);
