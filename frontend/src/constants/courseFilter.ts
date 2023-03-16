1bd4040828b69ca8962aede61871dae79e8dfda5
Jinwook Kim <kimjinwook94@gmail.com>
2023-03-17 00:41:54 +0900

    [FE] 개발 분야, 기술 스택 옵션 및 정렬 수정
    - 중요하지 않은 개발 분야 및 스택 삭제
    - 중요도 순으로 정렬 수정

--- frontend/src/constants/courseFilter.ts
+++ frontend/src/constants/courseFilter.ts
@@ -1,14 +1,14 @@
 export const SIDE_FILTER = 'SIDE_FILTER';
 export const MODAL_FILTER = 'MODAL_FILTER';
 
-const CATEGORIES_OPTIONS = {
-  SUPER: ['웹', '모바일 앱', '게임', '임베디드', '데스크탑 앱', 'AI', '데이터', '데브옵스', '보안'],
-  SUB: ['프론트엔드', '백엔드', '풀스택', '안드로이드', 'iOS', '크로스플랫폼'],
+export const CATEGORIES_OPTIONS = {
+  SUPER: ['웹', '모바일 앱', 'AI', '데이터', '데브옵스', '게임'],
+  SUB: ['프론트엔드', '백엔드', '풀스택', '안드로이드', 'iOS'],
 };
 
-const STACKS_OPTIONS = {
-  LANGUAGES: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Java', 'Python', 'Kotlin', 'Swift'],
-  FRAMEWORKS: ['Spring', 'Django', 'React', 'Vue.js', 'Node.js', 'Angular'],
+export const STACKS_OPTIONS = {
+  LANGUAGES: ['JavaScript', 'TypeScript', 'Java', 'Python', 'Swift', 'Kotlin'],
+  FRAMEWORKS: ['React', 'Vue.js', 'Spring', 'Node.js', 'Django'],
 };
 
 export const CATEGORIES = {
@@ -22,6 +22,7 @@ export const STACKS = {
   filterOptions: [...STACKS_OPTIONS.LANGUAGES, ...STACKS_OPTIONS.FRAMEWORKS],
   isMore: true,
 };
+
 export const COST_TYPE = { filterName: '수강 비용', filterOptions: ['무료', '무료 (국비)'], isMore: false };
 export const PERIOD = { filterName: '수강 기간', filterOptions: [], isMore: false };
 export const TEST = { filterName: '코딩 테스트', filterOptions: ['있음', '없음'], isMore: false };
