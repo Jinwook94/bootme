d4e0bcfc8123316681fa8555b1c5774eebe705a0
Jinwook Kim <kimjinwook94@gmail.com>
2023-04-04 13:59:21 +0900

    feat: AdminController -> CourseController, CompanyController 분리

---
+++ backend/src/main/java/com/bootme/course/controller/CourseController.java
@@ -0,0 +1,63 @@
+package com.bootme.course.controller;
+
+import com.bootme.course.dto.CourseRequest;
+import com.bootme.course.dto.CourseResponse;
+import com.bootme.course.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+@RequiredArgsConstructor
+public class CourseController {
+
+    public final CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<CourseResponse> addCourse(@Valid @RequestBody CourseRequest courseRequest){
+        Long courseId = courseService.addCourse(courseRequest);
+        CourseResponse courseResponse = courseService.findById(courseId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/courses/" + courseId);
+        return new ResponseEntity<>(courseResponse, headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseResponse> findCourse(@PathVariable Long id){
+        CourseResponse courseResponse = courseService.findById(id);
+        return ResponseEntity.ok(courseResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseResponse>> findAllCourses(){
+        List<CourseResponse> courseResponses = courseService.findAll();
+        int size = courseResponses.size();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Expose-Headers", "X-Total-Count");
+        headers.add("X-Total-Count", String.valueOf(size));
+        headers.add("Content-Range", "courses 0-"+size+"/"+size);
+
+        return ResponseEntity.ok().headers(headers).body(courseResponses);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> modifyCourse(@PathVariable Long id,
+                                             @Valid @RequestBody CourseRequest courseRequest){
+        courseService.modifyCourse(id, courseRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long id){
+        courseService.deleteCourse(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
