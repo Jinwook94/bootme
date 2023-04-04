426599f0946f03be391372363ae464ee7558ee41
Jinwook Kim <kimjinwook94@gmail.com>
2023-04-04 14:19:18 +0900

    feat: AdminControllerTest -> CourseControllerTest, CompanyControllerTest 분리

---
+++ backend/src/test/java/com/bootme/course/controller/CompanyControllerTest.java
@@ -0,0 +1,143 @@
+package com.bootme.course.controller;
+
+import com.bootme.course.dto.CompanyResponse;
+import com.bootme.course.service.CompanyService;
+import com.bootme.util.ControllerTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.bootme.util.fixture.CourseFixture.getCompanyRequest;
+import static com.bootme.util.fixture.CourseFixture.getCompanyResponse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CompanyController.class)
+@DisplayName("CompanyController 클래스의")
+class CompanyControllerTest extends ControllerTest {
+
+    @MockBean
+    private CompanyService companyService;
+
+    @Test
+    @DisplayName("addCompany()는 정상 요청시 회사를 추가하고 상태코드 201을 반환한다.")
+    void addCompany() throws Exception {
+        //given
+        String content = objectMapper.writeValueAsString(getCompanyRequest(1));
+        given(companyService.addCompany(any())).willReturn(1L);
+        given(companyService.findById(1L)).willReturn(getCompanyResponse(1));
+
+        //when
+        ResultActions perform = mockMvc.perform(post("/companies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content));
+
+        //then
+        perform.andExpect(status().isCreated());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("companies/add/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("findCompany()는 정상 요청시 상태코드 200을 반환한다.")
+    void findCompany() throws Exception {
+        //given
+        given(companyService.findById(any())).willReturn(getCompanyResponse(1));
+
+        //when
+        ResultActions perform = mockMvc.perform(get("/companies/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        perform.andExpect(status().isOk());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("companies/find/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("findCompanies()는 정상 요청시 상태코드 200을 반환한다.")
+    void findCompanies() throws Exception {
+        //given
+        List<CompanyResponse> companyResponses = new ArrayList<>();
+        companyResponses.add(getCompanyResponse(1));
+        companyResponses.add(getCompanyResponse(2));
+        given(companyService.findAll()).willReturn(companyResponses);
+
+        //when
+        ResultActions perform = mockMvc.perform(get("/companies")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        perform.andExpect(status().isOk());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("companies/findAll/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("modifyCompany()는 정상 요청시 상태코드 204를 반환한다.")
+    void modifyCompany() throws Exception{
+        //given
+        String content = objectMapper.writeValueAsString(getCompanyRequest(1));
+        willDoNothing().given(companyService).modifyCompany(any(), any());
+
+        //when
+        ResultActions perform = mockMvc.perform(put("/companies/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content));
+
+        //then
+        perform.andExpect(status().isNoContent());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("companies/modify/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("deleteCompany()는 정상 요청시 상태코드 204를 반환한다.")
+    void deleteCompany() throws Exception {
+        //given
+        willDoNothing().given(companyService).deleteCompany(any());
+
+        //when
+        ResultActions perform = mockMvc.perform(delete("/companies/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        perform.andExpect(status().isNoContent());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("companies/delete/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+}
---
+++ backend/src/test/java/com/bootme/course/controller/CourseControllerTest.java
@@ -0,0 +1,145 @@
+package com.bootme.course.controller;
+
+import com.bootme.course.dto.CourseResponse;
+import com.bootme.course.service.CourseService;
+import com.bootme.util.ControllerTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.bootme.util.fixture.CourseFixture.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CourseController.class)
+@DisplayName("CourseController 클래스의")
+class CourseControllerTest extends ControllerTest {
+
+    @MockBean
+    private CourseService courseService;
+
+    @Test
+    @DisplayName("addCourse()는 정상 요청시 코스를 추가하고 상태코드 201을 반환한다.")
+    void addCourse() throws Exception {
+        //given
+        String content = objectMapper.writeValueAsString(getCourseRequest(1));
+        given(courseService.addCourse(any())).willReturn(1L);
+        given(courseService.findById(1L)).willReturn(getCourseResponse(1));
+
+        //when
+        ResultActions perform = mockMvc.perform(post("/courses")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content));
+
+        //then
+        perform.andExpect(status().isCreated());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("courses/add/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("findCourse()는 정상 요청시 상태코드 200을 반환한다.")
+    void findCourse() throws Exception {
+        //given
+        given(courseService.findById(any())).willReturn(getCourseResponse(1));
+
+        //when
+        ResultActions perform = mockMvc.perform(get("/courses/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        perform.andExpect(status().isOk());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("courses/find/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("findCourses()는 정상 요청시 상태코드 200을 반환한다.")
+    void findCourses() throws Exception {
+        //given
+        List<CourseResponse> courseResponses = new ArrayList<>();
+        courseResponses.add(getCourseResponse(1));
+        courseResponses.add(getCourseResponse(2));
+        courseResponses.add(getCourseResponse(3));
+        given(courseService.findAll()).willReturn(courseResponses);
+
+        //when
+        ResultActions perform = mockMvc.perform(get("/courses")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        perform.andExpect(status().isOk());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("courses/findAll/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+
+    }
+
+    @Test
+    @DisplayName("modifyCourse()는 정상 요청시 상태코드 204를 반환한다.")
+    void modifyCourse() throws Exception{
+        //given
+        String content = objectMapper.writeValueAsString(getCourseRequest(1));
+        willDoNothing().given(courseService).modifyCourse(any(), any());
+
+        //when
+        ResultActions perform = mockMvc.perform(put("/courses/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content));
+
+        //then
+        perform.andExpect(status().isNoContent());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("courses/modify/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("deleteCourse()는 정상 요청시 상태코드 204를 반환한다.")
+    void deleteCourse() throws Exception {
+        //given
+        willDoNothing().given(courseService).deleteCourse(any());
+
+        //when
+        ResultActions perform = mockMvc.perform(delete("/courses/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        perform.andExpect(status().isNoContent());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("courses/delete/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+}
