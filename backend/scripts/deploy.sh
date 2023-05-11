6cb117ad6c3027f79bf0738126232a0aea445911
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-11 13:59:43 +0900

    [BE] 배포 스크립트(deploy.sh) 수정
    - jar 파일 실행시 로그 파일 저장 경로 설정 안함. logback 설정파일에서 로그 파일 저장 경로 설정하기 때문에
    
    - jar 파일 실행 전후로 스프링 부트 어플리케이션을 실행하고 실행 완료한다는 내용을 전달하는 실행 로그가 user-data.log 파일에 기록되도록 함

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -4,5 +4,6 @@ REPOSITORY=/home/ec2-user/deployment
 cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "bootme" | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /home/ec2-user/deployment/logfile.log 2>&1 &
+echo ">Starting spring boot application: $JAR_PATH" > /var/log/user-data.log
+nohup java -jar $JAR_PATH
+echo ">Spring boot application has been started: $JAR_PATH " > /var/log/user-data.log
