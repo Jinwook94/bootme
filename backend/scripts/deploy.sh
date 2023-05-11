ec54b5aa769f52e40f08321ce7e775b79bafb584
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-11 14:55:02 +0900

    [BE] 배포 스크립트(deploy.sh) 수정
    - user-data.log 파일 권한 부여 커맨드 추가
    - 스프링 부트 실행 로그 출력 형식 수정 (실행 시간 추가 등)

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -4,6 +4,7 @@ REPOSITORY=/home/ec2-user/deployment
 cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "bootme" | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-echo ">Starting spring boot application: $JAR_PATH" > /var/log/user-data.log
+sudo chmod 777 /var/log/user-data.log
+echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Starting Spring Boot Application: $JAR_PATH" >> /var/log/user-data.log
 nohup java -jar $JAR_PATH
-echo ">Spring boot application has been started: $JAR_PATH " > /var/log/user-data.log
+echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Spring Boot Application has been started: $JAR_PATH " >> /var/log/user-data.log
