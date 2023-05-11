aad47f59d3e4218e3dfebd0894bd67de7f3c05cc
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-11 15:22:09 +0900

    [BE] 배포 스크립트(deploy.sh) 수정
    - jar 실행 커맨드에 & 추가
    - 설명:
    스크립트에서 nohup java -jar $JAR_PATH 명령은 백그라운드에서 실행되지만, & 기호를 사용하여 명시적으로 백그라운드로 보내지 않으면, 스크립트의 실행은 이 프로세스가 완료될 때까지 일시 중지됩니다. 따라서 nohup 명령 바로 아래의 echo 명령은 Spring Boot 애플리케이션이 완전히 종료된 후에야 실행됩니다.
    
    이 문제를 해결하려면, nohup 명령을 실행한 후에 명시적으로 백그라운드로 보내야 합니다. 이는 nohup java -jar $JAR_PATH &와 같이 &를 추가하여 수행할 수 있습니다. 이렇게 하면 nohup 명령이 백그라운드에서 실행되고 스크립트는 다음 명령인 echo로 즉시 진행합니다.

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -6,5 +6,5 @@ JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "bootme" | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 sudo chmod 777 /var/log/user-data.log
 echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Starting Spring Boot Application: $JAR_PATH" >> /var/log/user-data.log
-nohup java -jar $JAR_PATH
+nohup java -jar $JAR_PATH &
 echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Spring Boot Application has been started: $JAR_PATH " >> /var/log/user-data.log
