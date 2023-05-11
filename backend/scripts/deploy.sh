988cd12fd12d88f9a873ec691ec330672b82e640
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-11 16:26:47 +0900

    [BE] 배포 스크립트(deploy.sh) 수정: logs 폴더 권한 부여
    - 스프링부트 어플리케이션에서 logs 폴더에 파일 생성 권한이 없어서 에러 발생
    - chmod 777 커맨드로 권한 부여

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -5,6 +5,9 @@ cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "bootme" | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 sudo chmod 777 /var/log/user-data.log
+sudo chmod 777 /home/ec2-user/logs
+sudo chmod 777 /home/ec2-user/logs/general
+sudo chmod 777 /home/ec2-user/logs/access
 echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Starting Spring Boot Application: $JAR_PATH" >> /var/log/user-data.log
-nohup java -jar $JAR_PATH &
+nohup java -jar $JAR_PATH > /home/ec2-user/logs/nohup.out 2>&1 &&
 echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Spring Boot Application has been started: $JAR_PATH " >> /var/log/user-data.log
