33b57734504cc3db57f575018bc69fadb4257189
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2023-05-16 18:56:00 +0900

    [BE] 로깅 설정 (logback, LoggingAspect) (#223)
    * feat: JSON 형식 로그 편리하게 저장하기 위해 의존성 추가
    
    https://github.com/logfellow/logstash-logback-encoder
    
    * feat: 엑세스 로그에서 요청 바디를 출력하기 위해 TeeFilter 등록
    
    https://logback.qos.ch/manual/layouts.html#requestContent
    
    * feat: MDC 필터 작성
    
    https://mangkyu.tistory.com/266
    https://logback.qos.ch/manual/mdc.html
    
    * feat: LoggingAspect 로그 출력 형식 변경
    
    * feat: logback 설정 변경

---
+++ backend/src/main/java/com/bootme/config/AccessLogConfig.java
@@ -0,0 +1,21 @@
+package com.bootme.config;
+
+import ch.qos.logback.access.servlet.TeeFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 엑세스 로그에서 요청 바디를 출력하기 위해 TeeFilter 등록 필요하여 작성 <br>
+ * 엑세스 로그 설정 파일(file-access-logger.xml)의 로그 출력 패턴 참고 <br><br>
+ *
+ * 참고: <a href="https://logback.qos.ch/manual/layouts.html#requestContent:~:text=of%20the%20response.-,requestContent,-This%20conversion%20word">Logback documentation</a>, <a href="https://ozymaxx.github.io/blog/2020/06/19/logback-access-en/#:~:text=We%20went%20really,ourselves%20(here)%3A">TeeFilter 관련 아티클</a>
+ */
+@Configuration
+public class AccessLogConfig {
+    @Bean
+    public FilterRegistrationBean requestLogFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new TeeFilter());
+        return filterRegistrationBean;
+    }
+}
