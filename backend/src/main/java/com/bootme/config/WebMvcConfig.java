1dff5e4f96c5944053edf856855d4c1b878d3443
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-18 13:37:44 +0900

    feat: 토큰 검증 인터셉터 등록 (WebMvcConfig)

--- backend/src/main/java/com/bootme/config/WebMvcConfig.java
+++ backend/src/main/java/com/bootme/config/WebMvcConfig.java
@@ -1,13 +1,21 @@
 package com.bootme.config;
 
+import com.bootme.common.interceptor.TokenValidationInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,DELETE,TRACE,OPTIONS,PATCH,PUT";
+
+    private final TokenValidationInterceptor tokenValidationInterceptor;
+
+    public WebMvcConfig(TokenValidationInterceptor tokenValidationInterceptor) {
+        this.tokenValidationInterceptor = tokenValidationInterceptor;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -19,5 +27,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenValidationInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login")
+                .excludePathPatterns("/logout");
     }
 }
