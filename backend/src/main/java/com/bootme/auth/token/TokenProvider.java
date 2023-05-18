580d4e094f2c177dbff532d651a8126c7b1f39bf
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-18 14:28:49 +0900

    [FE] TokenProvider 리팩토링
    1. 토큰 만료시간 변수명 변경
    - validityInMilliseconds > accessTokenExpireTimeInMilliseconds
    - refreshTokenValidityMilliseconds > refreshTokenExpireTimeInMilliseconds
    
    2. 토큰 생성 중복 코드 제거
    - createToken() 함수를 재사용하여 중복 제거
    
    3. isValid() 예외 처리 수정
    - MalformedJwtException, ExpiredJwtException 예외 발생시 커스텀 처리 불필요하여 JwtException 만 캐치하여 처리

--- backend/src/main/java/com/bootme/auth/token/TokenProvider.java
+++ backend/src/main/java/com/bootme/auth/token/TokenProvider.java
@@ -2,6 +2,7 @@ package com.bootme.auth.token;
 
 import com.bootme.auth.dto.JwtVo;
 import com.bootme.member.domain.Member;
+import com.bootme.member.exception.MemberNotFoundException;
 import com.bootme.member.repository.MemberRepository;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
@@ -13,49 +14,44 @@ import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
+import static com.bootme.common.exception.ErrorType.NOT_FOUND_MEMBER;
+
 @Component
 @Slf4j
 public class TokenProvider {
 
     private final String issuer;
     private final Key signingKey;
-    private final long validityInMilliseconds;
-    private final long refreshTokenValidityMilliseconds;
+    private final long accessTokenExpireTimeInMilliseconds;
+    private final long refreshTokenExpireTimeInMilliseconds;
     private final MemberRepository memberRepository;
 
     public TokenProvider(@Value("${security.jwt.bootme.issuer}") String issuer,
                          @Value("${security.jwt.bootme.secret-key}") String secretKey,
-                         @Value("${security.jwt.bootme.exp.millisecond.access}") long validityInMilliseconds,
-                         @Value("${security.jwt.bootme.exp.millisecond.refresh}") long refreshTokenValidityMilliseconds,
+                         @Value("${security.jwt.bootme.exp.millisecond.access}") long accessTokenExpireTimeInMilliseconds,
+                         @Value("${security.jwt.bootme.exp.millisecond.refresh}") long refreshTokenExpireTimeInMilliseconds,
                          MemberRepository memberRepository) {
         byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
         this.issuer = issuer;
         this.signingKey = Keys.hmacShaKeyFor(keyBytes);
-        this.validityInMilliseconds = validityInMilliseconds;
-        this.refreshTokenValidityMilliseconds = refreshTokenValidityMilliseconds;
+        this.accessTokenExpireTimeInMilliseconds = accessTokenExpireTimeInMilliseconds;
+        this.refreshTokenExpireTimeInMilliseconds = refreshTokenExpireTimeInMilliseconds;
         this.memberRepository = memberRepository;
     }
 
     public String createAccessToken(JwtVo.Body body) {
-        Member member = memberRepository.findByEmail(body.getEmail()).orElseThrow();
-        Date now = new Date();
-        Date expiration = new Date(now.getTime() + validityInMilliseconds);
-
-        return Jwts.builder()
-                .setHeaderParam("typ", "JWT")
-                .claim("id", member.getId())
-                .claim("email", member.getEmail())
-                .setIssuer(issuer)
-                .setIssuedAt(now)
-                .setExpiration(expiration)
-                .signWith(signingKey)
-                .compact();
+        return createToken(body, accessTokenExpireTimeInMilliseconds);
     }
 
     public String createRefreshToken(JwtVo.Body body) {
-        Member member = memberRepository.findByEmail(body.getEmail()).orElseThrow();
+        return createToken(body, refreshTokenExpireTimeInMilliseconds);
+    }
+
+    private String createToken(JwtVo.Body body, long validityInMilliseconds) {
+        Member member = memberRepository.findByEmail(body.getEmail())
+                .orElseThrow(() -> new MemberNotFoundException(NOT_FOUND_MEMBER, body.getEmail()));
         Date now = new Date();
-        Date expiration = new Date(now.getTime() + refreshTokenValidityMilliseconds);
+        Date expiration = new Date(now.getTime() + validityInMilliseconds);
 
         return Jwts.builder()
                 .setHeaderParam("typ", "JWT")
@@ -69,10 +65,12 @@ public class TokenProvider {
     }
 
     public String reissueAccessToken(String refreshToken){
-        Long memberId = parseToken(refreshToken).get("id", Long.class);
-        String memberEmail = parseToken(refreshToken).get("email", String.class);
+        Claims claims = parseToken(refreshToken);
+        Long memberId = claims.get("id", Long.class);
+        String memberEmail = claims.get("email", String.class);
+
         Date now = new Date();
-        Date expiration = new Date(now.getTime() + validityInMilliseconds);
+        Date expiration = new Date(now.getTime() + accessTokenExpireTimeInMilliseconds);
 
         return Jwts.builder()
                 .setHeaderParam("typ", "JWT")
@@ -101,12 +99,6 @@ public class TokenProvider {
                     .parseClaimsJws(token);
 
             return !claims.getBody().getExpiration().before(new Date());
-        } catch (MalformedJwtException e) {
-            log.error("Malformed JWT: {}", e.getMessage());
-            return false;
-        } catch (ExpiredJwtException e) {
-            log.warn("Expired JWT: {}", e.getMessage());
-            return false;
         } catch (JwtException e) {
             log.warn("JWT Exception: {}", e.getMessage());
             return false;
