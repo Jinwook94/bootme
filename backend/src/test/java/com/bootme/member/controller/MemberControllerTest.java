cc3f5a28ece09a660d78734d0a0ea999f56cc06e
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-20 01:46:14 +0900

    [BE] MemberControllerTest 작성

---
+++ backend/src/test/java/com/bootme/member/controller/MemberControllerTest.java
@@ -0,0 +1,95 @@
+package com.bootme.member.controller;
+
+import com.bootme.auth.token.TokenProvider;
+import com.bootme.member.service.MemberService;
+import com.bootme.util.ControllerTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberController.class)
+@DisplayName("MemberController 클래스의")
+class MemberControllerTest extends ControllerTest {
+
+    @MockBean
+    private MemberService memberService;
+
+    @MockBean
+    private TokenProvider tokenProvider;
+
+    @Test
+    @DisplayName("addBookmarkCourse()는 정상 요청시 상태코드 201을 반환한다.")
+    void addBookmarkCourse() throws Exception {
+        //given
+        given(memberService.addBookmarkCourse(anyLong(), anyLong())).willReturn(1L);
+
+        //when
+        ResultActions perform = mockMvc.perform(post("/member/1/bookmarks/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        perform.andExpect(status().isCreated());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("member/addBookmarkCourse/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("deleteBookmarkCourse()는 정상 요청시 상태코드 204를 반환한다.")
+    void deleteBookmarkCourse() throws Exception {
+        //given
+        willDoNothing().given(memberService).deleteBookmarkCourse(anyLong(), anyLong());
+
+        //when
+        ResultActions perform = mockMvc.perform(delete("/member/1/bookmarks/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        perform.andExpect(status().isNoContent());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("member/deleteBookmarkCourse/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("findAllBookmarkCourses()는 정상 요청시 상태코드 200을 반환한다.")
+    void findAllBookmarkCourses() throws Exception {
+        //given
+        List<Long> bookmarkCourses = Arrays.asList(1L, 2L, 3L);
+        given(memberService.findBookmarkCourseByMemberId(anyLong())).willReturn(bookmarkCourses);
+
+        //when
+        ResultActions perform = mockMvc.perform(get("/member/1/bookmarks")
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        perform.andExpect(status().isOk());
+
+        //docs
+        perform.andDo(print())
+                .andDo(document("member/findAllBookmarkCourses/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+}
