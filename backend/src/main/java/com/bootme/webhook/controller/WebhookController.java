010c0aff4690c6ec53a30730556f26680b3b3737
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-21 00:40:43 +0900

    [BE] WebhookControllerTest 실패 케이스 작성

--- backend/src/main/java/com/bootme/webhook/controller/WebhookController.java
+++ backend/src/main/java/com/bootme/webhook/controller/WebhookController.java
@@ -4,7 +4,6 @@ import com.bootme.auth.service.AuthService;
 import com.bootme.common.exception.ValidationException;
 import com.bootme.course.service.CourseService;
 import com.bootme.webhook.dto.WebhookRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,42 +16,39 @@ import static com.bootme.common.exception.ErrorType.INVALID_EVENT;
 @RequestMapping("/webhook")
 public class WebhookController {
 
+    private static final String COURSE_CLICKED = "courseClicked";
+    private static final String COURSE_BOOKMARKED = "courseBookmarked";
+
     private final AuthService authService;
     private final CourseService courseService;
 
-    public WebhookController(AuthService authService,
-                             CourseService courseService) {
+    public WebhookController(AuthService authService, CourseService courseService) {
         this.authService = authService;
         this.courseService = courseService;
     }
 
     @PostMapping
     public ResponseEntity<String> handleWebhook(@RequestHeader(name = "Bootme_Secret") String secret,
-                                           @RequestBody WebhookRequest webhookRequest) {
-        final String COURSE_CLICKED = "courseClicked";
-        final String COURSE_BOOKMARKED = "courseBookmarked";
-        final String EVENT = webhookRequest.getEvent();
-        final Map<String, String> data = webhookRequest.getData();
-        final Long COURSE_ID = Long.parseLong(data.get("courseId"));
-        final String jwt = authService.getToken(secret);
-
+                                                @RequestBody WebhookRequest webhookRequest) {
+        String jwt = authService.getToken(secret);
         authService.verifyToken(jwt);
+        processWebhookEvent(webhookRequest.getEvent(), webhookRequest.getData());
+        return ResponseEntity.ok().build();
+    }
 
-        try {
-            switch (EVENT) {
-                case COURSE_CLICKED:
-                    courseService.incrementClicks(COURSE_ID);
-                    break;
-                case COURSE_BOOKMARKED:
-                    courseService.incrementBookmarks(COURSE_ID);
-                    break;
-                default:
-                    throw new ValidationException(INVALID_EVENT, webhookRequest.getEvent());
-            }
-        } catch (ValidationException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    private void processWebhookEvent(String event, Map<String, String> data) {
+        switch (event) {
+            case COURSE_CLICKED:
+                long courseId = Long.parseLong(data.get("courseId"));
+                courseService.incrementClicks(courseId);
+                break;
+            case COURSE_BOOKMARKED:
+                courseId = Long.parseLong(data.get("courseId"));
+                courseService.incrementBookmarks(courseId);
+                break;
+            default:
+                throw new ValidationException(INVALID_EVENT, event);
         }
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
--- backend/src/test/java/com/bootme/webhook/controller/WebhookControllerTest.java
+++ backend/src/test/java/com/bootme/webhook/controller/WebhookControllerTest.java
@@ -4,6 +4,7 @@ import com.bootme.auth.service.AuthService;
 import com.bootme.auth.token.TokenProvider;
 import com.bootme.course.service.CourseService;
 import com.bootme.util.ControllerTest;
+import com.bootme.webhook.dto.WebhookRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,14 +12,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.bootme.common.exception.ErrorType.INVALID_EVENT;
 import static com.bootme.util.fixture.WebhookFixture.getWebhookRequest;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -36,13 +41,11 @@ class WebhookControllerTest extends ControllerTest {
     private TokenProvider tokenProvider;
 
     @Test
-    @DisplayName("handleWebhook() 정상 요청시 상태코드 200을 반환한다.")
+    @DisplayName("handleWebhook()는 정상 요청시 상태코드 200을 반환한다.")
     void handleWebhook() throws Exception {
         // given
         String content = objectMapper.writeValueAsString(getWebhookRequest(1));
         willDoNothing().given(authService).verifyToken(anyString());
-        willDoNothing().given(courseService).incrementClicks(anyLong());
-        willDoNothing().given(courseService).incrementBookmarks(anyLong());
 
         // when
         ResultActions perform = mockMvc.perform(post("/webhook")
@@ -56,6 +59,41 @@ class WebhookControllerTest extends ControllerTest {
         // docs
         perform.andDo(print())
                 .andDo(document("webhook/handleWebhook/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("handleWebhook()는 유효한 웹훅 이벤트가 아닌 경우 400 Bad Request 를 반환한다.")
+    void handleWebhook_INVALID_EVENT() throws Exception {
+        // given
+        Map<String, String> data = new HashMap<>() {{ put("oAuthProvider", "naver");}};
+        WebhookRequest webhookRequest = WebhookRequest.builder()
+                                            .sessionId(1L)
+                                            .memberId(1L)
+                                            .event("login")
+                                            .data(data)
+                                            .currentUrl("https://bootme.co.kr/")
+                                            .createdAt(1684335600L)
+                                            .build();
+        String content = objectMapper.writeValueAsString(webhookRequest);
+        willDoNothing().given(authService).verifyToken(any());
+
+        // when
+        ResultActions perform = mockMvc.perform(post("/webhook")
+                .header("Bootme_Secret", "secret")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content));
+
+        // then
+        perform.andExpectAll(
+                status().isBadRequest(),
+                jsonPath("message").value(INVALID_EVENT.getMessage())
+        );
+
+        // docs
+        perform.andDo(print())
+                .andDo(document("webhook/handleWebhook/fail/invalid-event",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint())));
     }
