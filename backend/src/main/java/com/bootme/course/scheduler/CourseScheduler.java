d7d95430179c7fb3c816edd5953feaf3c7168e26
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-21 16:12:55 +0900

    [BE] CourseScheduler 리팩토링: 중복 코드 제거, 로깅 추가

--- backend/src/main/java/com/bootme/course/scheduler/CourseScheduler.java
+++ backend/src/main/java/com/bootme/course/scheduler/CourseScheduler.java
@@ -8,6 +8,7 @@ import com.bootme.member.repository.BookmarkCourseRepository;
 import com.bootme.notification.domain.Notification;
 import com.bootme.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,11 +16,16 @@ import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class CourseScheduler {
 
     private final CourseRepository courseRepository;
     private final BookmarkCourseRepository bookmarkCourseRepository;
     private final NotificationRepository notificationRepository;
+
+    private static final String REGISTRATION_START = "registrationStart";
+    private static final String REGISTRATION_END_IN_THREE_DAYS = "registrationEndInThreeDays";
+    private static final String REGISTRATION_END = "registrationEnd";
 
     @Scheduled(cron = "00 55 09 * * *", zone = "Asia/Seoul")
     public void updateIsRegisterOpen() {
@@ -30,57 +36,47 @@ public class CourseScheduler {
                 courseRepository.updateIsRegisterOpen(course.getId(), updatedStatus);
             }
         }
+        log.info("@Scheduled 코스 접수중 여부 업데이트 완료");
     }
 
     @Scheduled(cron = "00 00 10 * * *", zone = "Asia/Seoul")
     public void notifyCourseRegistrationStart() {
-        final String EVENT = "registrationStart";
-        LocalDate now = LocalDate.now();
-        Iterable<BookmarkCourse> bookmarkCourses = bookmarkCourseRepository.findAll();
-
-        for (BookmarkCourse bookmarkCourse : bookmarkCourses) {
-            Member member = bookmarkCourse.getMember();
-            Course course = bookmarkCourse.getCourse();
-            boolean registrationStartsToday = course.getDates().isRegistrationStartsOn(now);
-            if (registrationStartsToday) {
-                Notification notification = Notification.of(member, EVENT, bookmarkCourse);
-                notificationRepository.save(notification);
-            }
-        }
+        notifyBookmarkCourses(REGISTRATION_START, LocalDate.now());
+        log.info("@Scheduled 코스 접수 시작 알림 전송 완료");
     }
 
     @Scheduled(cron = "00 00 10 * * *", zone = "Asia/Seoul")
     public void notifyCourseRegistrationEndInThreeDays() {
-        final String EVENT = "registrationEndInThreeDays";
-        LocalDate threeDaysLater = LocalDate.now().plusDays(3);
-        Iterable<BookmarkCourse> bookmarkCourses = bookmarkCourseRepository.findAll();
-
-        for (BookmarkCourse bookmarkCourse : bookmarkCourses) {
-            Member member = bookmarkCourse.getMember();
-            Course course = bookmarkCourse.getCourse();
-            boolean registrationEndsInThreeDays = course.getDates().isRegistrationEndsOn(threeDaysLater);
-            if (registrationEndsInThreeDays) {
-                Notification notification = Notification.of(member, EVENT, bookmarkCourse);
-                notificationRepository.save(notification);
-            }
-        }
+        notifyBookmarkCourses(REGISTRATION_END_IN_THREE_DAYS, LocalDate.now().plusDays(3));
+        log.info("@Scheduled 코스 접수 마감 3일전 알림 전송 완료");
     }
 
     @Scheduled(cron = "00 00 10 * * *", zone = "Asia/Seoul")
     public void notifyCourseRegistrationEnd() {
-        final String EVENT = "registrationEnd";
-        LocalDate now = LocalDate.now();
+        notifyBookmarkCourses(REGISTRATION_END, LocalDate.now());
+        log.info("@Scheduled 코스 접수 마감 당일 알림 전송 완료");
+    }
+
+    private void notifyBookmarkCourses(String event, LocalDate date) {
         Iterable<BookmarkCourse> bookmarkCourses = bookmarkCourseRepository.findAll();
 
         for (BookmarkCourse bookmarkCourse : bookmarkCourses) {
             Member member = bookmarkCourse.getMember();
             Course course = bookmarkCourse.getCourse();
-            boolean registrationEndsToday = course.getDates().isRegistrationEndsOn(now);
-            if (registrationEndsToday) {
-                Notification notification = Notification.of(member, EVENT, bookmarkCourse);
+
+            boolean isEventToday = false;
+            if (REGISTRATION_START.equals(event)) {
+                isEventToday = course.getDates().isRegistrationStartsOn(date);
+            } else if (REGISTRATION_END_IN_THREE_DAYS.equals(event)) {
+                isEventToday = course.getDates().isRegistrationEndsOn(date);
+            } else if (REGISTRATION_END.equals(event)) {
+                isEventToday = course.getDates().isRegistrationEndsOn(date);
+            }
+
+            if (isEventToday) {
+                Notification notification = Notification.of(member, event, bookmarkCourse);
                 notificationRepository.save(notification);
             }
         }
     }
-
 }
