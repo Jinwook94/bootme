29b8537c7866db2836483616182c0a2575fc8af4
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-21 15:53:44 +0900

    [BE] 스케줄러 작동 위해 @EnableScheduling 추가
    - CourseScheduler 클래스의 @Scheduled 작동시키기 위해 스프링 부트 어플리케이션에 @EnableScheduling 추가

--- backend/src/main/java/com/bootme/BootmeApplication.java
+++ backend/src/main/java/com/bootme/BootmeApplication.java
@@ -2,10 +2,12 @@ package com.bootme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BootmeApplication {
-    // Test
+
     public static void main(String[] args) {
         SpringApplication.run(BootmeApplication.class, args);
     }
