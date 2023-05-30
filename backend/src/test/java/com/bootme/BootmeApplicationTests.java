774e30d6e0dbdb7db60686455a3202c589008247
Jinwook Kim <kimjinwook94@gmail.com>
2023-05-30 18:53:04 +0900

    feat: 테스트시 dev 프로필 사용하도록 변경

--- backend/src/test/java/com/bootme/BootmeApplicationTests.java
+++ backend/src/test/java/com/bootme/BootmeApplicationTests.java
@@ -2,8 +2,10 @@ package com.bootme;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class BootmeApplicationTests {
 
     @Test
