e8c4097845019b0e58f971a968b60c5900001da6
Jinwook Kim <kimjinwook94@gmail.com>
2023-06-14 05:44:01 +0900

    [FE] babel 설정 롤백: 카카오톡 인앱 브라우저 오류 관련

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,8 +1,3 @@
-module.exports = function (api) {
-  api.cache(true);
-
-  return {
-    presets: [['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-env', '@babel/preset-typescript'],
-    plugins: ['macros'],
-  };
+module.exports = {
+  presets: [['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-env', '@babel/preset-typescript'],
 };
