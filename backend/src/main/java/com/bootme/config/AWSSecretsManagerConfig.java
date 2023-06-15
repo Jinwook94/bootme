e49292b8e1bb7d7d5d2a461fd08118dca38c6886
Jinwook Kim <kimjinwook94@gmail.com>
2023-06-15 22:09:08 +0900

    [BE] AWS 시크릿 개선: 시크릿을 빈으로 등록하여 사용, jasypt 사용하던 시크릿 값 AWS 시크릿 사용으로 변경

--- backend/src/main/java/com/bootme/config/AWSSecretsManagerConfig.java
+++ backend/src/main/java/com/bootme/config/AWSSecretsManagerConfig.java
@@ -5,8 +5,18 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.secretsmanager.caching.SecretCache;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.bootme.auth.dto.AwsSecrets;
+import com.bootme.common.exception.SerializationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.bootme.common.exception.ErrorType.JSON_PROCESSING_FAIL;
 
 @Configuration
 public class AWSSecretsManagerConfig {
@@ -22,6 +32,20 @@ public class AWSSecretsManagerConfig {
     @Bean
     public SecretCache secretCache(AWSSecretsManager awsSecretsManager) {
         return new SecretCache(awsSecretsManager);
+    }
+
+    @Bean
+    public AwsSecrets awsSecrets(SecretCache secretCache) {
+        String secretString = secretCache.getSecretString("/bootme/springboot");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> secretMap = new ConcurrentHashMap<>();
+        try {
+            secretMap = objectMapper.readValue(secretString, new TypeReference<>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new SerializationException(JSON_PROCESSING_FAIL, secretMap.toString(), e);
+        }
+        return AwsSecrets.of(secretMap);
     }
 
 }
