3dc907ba4ce2ce372463bc43ee4bedb2843ec769
Jinwook Kim <kimjinwook94@gmail.com>
2023-06-16 01:55:21 +0900

    [BE] BootmeApplicationTests에 AWS 관련 MockBean 추가

--- backend/src/test/java/com/bootme/BootmeApplicationTests.java
+++ backend/src/test/java/com/bootme/BootmeApplicationTests.java
@@ -1,10 +1,23 @@
 package com.bootme;
 
+import com.amazonaws.secretsmanager.caching.SecretCache;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.bootme.auth.dto.AwsSecrets;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class BootmeApplicationTests {
+
+    @MockBean
+    protected AWSSecretsManager awsSecretsManager;
+
+    @MockBean
+    protected SecretCache secretCache;
+
+    @MockBean
+    protected AwsSecrets awsSecrets;
 
     @Test
     void contextLoads() {
