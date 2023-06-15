e173e7bb4dad2cfa17831022d1b03cfd25bc0d2f
Jinwook Kim <kimjinwook94@gmail.com>
2023-06-16 03:17:18 +0900

    [BE] 시크릿 값 업데이트 위한 커밋

--- backend/src/main/java/com/bootme/BootmeApplication.java
+++ backend/src/main/java/com/bootme/BootmeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class BootmeApplication {
-    // CI CD
+
     public static void main(String[] args) {
         SpringApplication.run(BootmeApplication.class, args);
     }
