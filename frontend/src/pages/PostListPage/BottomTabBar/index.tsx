e9b340e8ea499eba9e412eea98ff5f8d294944b0
Jinwook Kim <kimjinwook94@gmail.com>
2023-06-17 03:24:23 +0900

    [FE] 게시글 리스트 페이지 하단 탭바 UI 문제 해결

--- frontend/src/pages/PostListPage/BottomTabBar/index.tsx
+++ frontend/src/pages/PostListPage/BottomTabBar/index.tsx
@@ -10,7 +10,7 @@ const BottomTapBar = () => {
   const { showSnackbar } = useSnackbar();
   return (
     <BottomTapBarWrapper>
-      <Link to={PATH.POST.LIST}>
+      <Link to={PATH.POST.LIST} style={{ width: '100%', height: '100%' }}>
         <TapBarItem>
           <TapBarItemIcon>
             {/*<HomeOutlined />*/}
@@ -19,13 +19,15 @@ const BottomTapBar = () => {
           <TapBarItemText>커뮤니티 홈</TapBarItemText>
         </TapBarItem>
       </Link>
-      <TapBarItem onClick={() => showSnackbar(SNACKBAR_MESSAGE.WORK_IN_PROGRESS, EXCLAMATION)}>
-        <TapBarItemIcon>
-          <BulbOutlined />
-        </TapBarItemIcon>
-        <TapBarItemText>디스커버리</TapBarItemText>
-      </TapBarItem>
-      <Link to={PATH.POST.WRITE}>
+      <a style={{ width: '100%', height: '100%', cursor: 'pointer' }}>
+        <TapBarItem onClick={() => showSnackbar(SNACKBAR_MESSAGE.WORK_IN_PROGRESS, EXCLAMATION)}>
+          <TapBarItemIcon>
+            <BulbOutlined />
+          </TapBarItemIcon>
+          <TapBarItemText>디스커버리</TapBarItemText>
+        </TapBarItem>
+      </a>
+      <Link to={PATH.POST.WRITE} style={{ width: '100%', height: '100%' }}>
         <TapBarItem>
           <TapBarItemIcon>
             <PlusOutlined />
@@ -33,13 +35,15 @@ const BottomTapBar = () => {
           <TapBarItemText>글쓰기</TapBarItemText>
         </TapBarItem>
       </Link>
-      <TapBarItem onClick={() => showSnackbar(SNACKBAR_MESSAGE.WORK_IN_PROGRESS, EXCLAMATION)}>
-        <TapBarItemIcon>
-          <CommentOutlined />
-        </TapBarItemIcon>
-        <TapBarItemText>채팅</TapBarItemText>
-      </TapBarItem>
-      <Link to={PATH.POST.SEARCH}>
+      <a style={{ width: '100%', height: '100%', cursor: 'pointer' }}>
+        <TapBarItem onClick={() => showSnackbar(SNACKBAR_MESSAGE.WORK_IN_PROGRESS, EXCLAMATION)}>
+          <TapBarItemIcon>
+            <CommentOutlined />
+          </TapBarItemIcon>
+          <TapBarItemText>채팅</TapBarItemText>
+        </TapBarItem>
+      </a>
+      <Link to={PATH.POST.SEARCH} style={{ width: '100%', height: '100%' }}>
         <TapBarItem>
           <TapBarItemIcon>
             <SearchOutlined />
