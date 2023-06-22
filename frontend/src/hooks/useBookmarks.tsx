6d23d76531f5f52dc164bac0f2d1b0f7eaeb3b18
Jinwook Kim <kimjinwook94@gmail.com>
2023-06-22 16:20:37 +0900

    [FE] handleBookmarkClick() 북마크 타입 인자로 받아서 처리

--- frontend/src/hooks/useBookmarks.tsx
+++ frontend/src/hooks/useBookmarks.tsx
@@ -4,7 +4,7 @@ import { useLogin } from './useLogin';
 import { useSnackbar } from './useSnackbar';
 import SNACKBAR_MESSAGE, { CHECK, EXCLAMATION } from '../constants/snackbar';
 import { BOOKMARK_TYPE } from '../constants/others';
-import { POST_BOOKMARKED, POST_CLICKED } from '../constants/webhook';
+import { COURSE_BOOKMARKED, COURSE_CLICKED, POST_BOOKMARKED, POST_CLICKED } from '../constants/webhook';
 import useWebhook from './useWebhook';
 
 const BookmarkContext = createContext<BookmarkContextProps>({
@@ -76,12 +76,18 @@ export const BookmarkProvider = ({ children }: BookmarkProviderProps) => {
     bookmarkedState: boolean,
     setBookmarkedState: React.Dispatch<boolean>
   ) => {
+    const eventTypes: Record<BookmarkType, { clicked: string; bookmarked: string }> = {
+      [BOOKMARK_TYPE.COURSE]: { clicked: COURSE_CLICKED, bookmarked: COURSE_BOOKMARKED },
+      [BOOKMARK_TYPE.POST]: { clicked: POST_CLICKED, bookmarked: POST_BOOKMARKED },
+    };
+    const eventType = eventTypes[type];
+
     if (isLogin) {
-      handleBookmark(id, BOOKMARK_TYPE.POST, bookmarkedState).then(() => {
+      handleBookmark(id, type, bookmarkedState).then(() => {
         setBookmarkedState(!bookmarkedState);
       });
-      sendWebhookNoti(POST_CLICKED, id);
-      sendWebhookNoti(POST_BOOKMARKED, id);
+      sendWebhookNoti(eventType.clicked, id);
+      sendWebhookNoti(eventType.bookmarked, id);
     } else {
       showSnackbar(SNACKBAR_MESSAGE.NEED_LOGIN, EXCLAMATION);
     }
