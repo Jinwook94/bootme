e38381f4c82121f7a40598df1311bd241567be79
Jinwook Kim <kimjinwook94@gmail.com>
2023-09-22 01:46:49 +0900

    [BE] data_count 테이블 관련 코드 삭제

--- backend/src/main/java/com/bootme/post/repository/PostRepository.java
+++ backend/src/main/java/com/bootme/post/repository/PostRepository.java
@@ -1,14 +1,10 @@
 package com.bootme.post.repository;
 
 import com.bootme.post.domain.Post;
-import com.bootme.post.domain.PostStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, QuerydslPredicateExecutor<Post> {
-
-    long countByStatus(PostStatus status);
-
 }
