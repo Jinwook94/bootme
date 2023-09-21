3d0e9b33e93f3bcad4e46c2425097f8475c3a00a
Jinwook Kim <kimjinwook94@gmail.com>
2023-09-21 23:37:09 +0900

    [BE] deploy.sh 파일 수정

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+sudo chmod 777 /var/log/user-data.log
+sudo chmod 777 /home/ec2-user/logs
 echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Starting deploy.sh" >> /var/log/user-data.log
 REPOSITORY=/home/ec2-user/deployment
 cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "bootme" | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-sudo chmod 777 /var/log/user-data.log
-sudo chmod 777 /home/ec2-user/logs
 echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Starting Spring Boot Application: $JAR_PATH" >> /var/log/user-data.log
 nohup java -jar $JAR_PATH > /home/ec2-user/logs/nohup.out 2>&1 &
