0c5cd66329ddfcdcc59b7b9f3429317e283f9e63
Jinwook Kim <kimjinwook94@gmail.com>
2023-09-24 21:51:37 +0900

    [BE] deploy.sh 파일 수정

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+sudo chmod 777 /var/log/user-data.log 2>> /var/log/user-data.log
+sudo chmod 777 /home/ec2-user/logs 2>> /var/log/user-data.log
 echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Starting deploy.sh" >> /var/log/user-data.log
-sudo chmod 777 /var/log/user-data.log
-sudo chmod 777 /home/ec2-user/logs
 REPOSITORY=/home/ec2-user/deployment
 cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "bootme" | tail -n 1)
