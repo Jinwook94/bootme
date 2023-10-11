dbde2eb5f35fe5c99a688e22a02d849ef03ec757
Jinwook Kim <kimjinwook94@gmail.com>
2023-10-12 01:41:11 +0900

    feat: 게시글 내용만 캐싱하기 위해 프록시 클래스에서 postRepository.findAll() 호출
    유저별 상태를 제외한 게시글 내용만 캐싱하기 위해 프록시 클래스에서 postRepository.findAll() 호출
    
    getPostPage()를 PostService 클래스에 구현하고 getPostPage()를 PostService 클래스에서 내부 호출 한다면, getPostPage()에 @Cacheable 붙여도 작동하지 않음 (Spring AOP, proxy 관련)
    
    그렇기 때문에 PostRepositoryProxy 클래스에 별도로 getPostPage() 구현

---
+++ backend/src/main/java/com/bootme/post/repository/PostRepositoryProxy.java
@@ -0,0 +1,51 @@
+package com.bootme.post.repository;
+
+import com.bootme.common.util.CustomPageImpl;
+import com.bootme.post.dto.PostResponse;
+import com.querydsl.core.types.Predicate;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import static com.bootme.common.enums.SortOption.CREATED_AT;
+import static com.bootme.common.enums.SortOption.LIKES;
+
+@Component
+@RequiredArgsConstructor
+public class PostRepositoryProxy {
+
+    private final PostRepository postRepository;
+
+    @Cacheable(value = "posts", key = "#size + ':' + #sort + ':' + (#topic == '' ? 'none' : #topic) + ':' + #page")
+    public CustomPageImpl<PostResponse> getPostPage(int page, int size, String sort, String topic, Predicate predicate) {
+        Pageable pageable = getSortedPageable(page, size, sort);
+        if (predicate == null) {
+            return new CustomPageImpl<>(postRepository.findAll(pageable)
+                    .map(post -> PostResponse.of(post, false, false)));
+        } else {
+            return new CustomPageImpl<>(postRepository.findAll(predicate, pageable)
+                    .map(post -> PostResponse.of(post, false, false)));
+        }
+    }
+
+    private Pageable getSortedPageable(int page, int size, String sort) {
+        Sort sorting;
+        if (sort.equals(CREATED_AT.toString())) {
+            sorting = Sort.by(
+                    Sort.Order.desc(CREATED_AT.toString()),
+                    Sort.Order.desc(LIKES.toString())
+            );
+        } else {
+            sorting = Sort.by(
+                    Sort.Order.desc(LIKES.toString()),
+                    Sort.Order.desc(CREATED_AT.toString())
+            );
+        }
+        return PageRequest.of(page-1, size, sorting);
+    }
+
+}
