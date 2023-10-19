4de206c0dc566121124d2d370cb87ce07ec5b050
Jinwook Kim <kimjinwook94@gmail.com>
2023-10-19 15:14:26 +0900

    [BE] 배포 자동화에 스프링 부트 실행시 Pinpoint Agent 설정 추가

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -7,5 +7,8 @@ REPOSITORY=/home/ec2-user/deployment
 cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "bootme" | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+JAVA_AGENT_PATH="/home/ec2-user/pinpoint/pinpoint-agent-2.5.2/pinpoint-bootstrap-2.5.2.jar"
+AGENT_ID="Agent-PROD-WAS-1"
+APP_NAME="BootMe-PROD-WAS"
 echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Starting Spring Boot Application: $JAR_PATH" >> /var/log/user-data.log
-nohup java -jar $JAR_PATH > /home/ec2-user/logs/nohup.out 2>&1 &
+nohup java -javaagent:$JAVA_AGENT_PATH -Dpinpoint.agentId=$AGENT_ID -Dpinpoint.applicationName=$APP_NAME -jar $JAR_PATH > /home/ec2-user/logs/nohup.out 2>&1 &
