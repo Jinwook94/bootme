b443d7a003479be42db90160aff2967fcb21c27d
Jinwook Kim <kimjinwook94@gmail.com>
2023-10-19 15:30:22 +0900

    [BE] 불필요한 cd 커맨드 제거 (deploy.sh)

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -4,7 +4,6 @@ sudo chmod 777 /var/log/user-data.log
 sudo chmod 777 /home/ec2-user/logs 2>> /var/log/user-data.log
 echo "$(date '+%Y-%m-%dT%H:%M:%S') INFO [deploy.sh] Starting deploy.sh" >> /var/log/user-data.log
 REPOSITORY=/home/ec2-user/deployment
-cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "bootme" | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 JAVA_AGENT_PATH="/home/ec2-user/pinpoint/pinpoint-agent-2.5.2/pinpoint-bootstrap-2.5.2.jar"
