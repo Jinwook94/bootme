8f2cf9abb9dece38f2b27d7d0c0ff3b435843c32
Jinwook Kim <kimjinwook94@gmail.com>
2023-10-21 18:08:57 +0900

    [BE] 프롬프트 API 문서 보충

--- backend/src/test/java/com/bootme/prompt/controller/PromptControllerTest.java
+++ backend/src/test/java/com/bootme/prompt/controller/PromptControllerTest.java
@@ -1,6 +1,7 @@
 package com.bootme.prompt.controller;
 
 import com.bootme.util.ControllerTest;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,11 +10,13 @@ import org.springframework.test.web.servlet.ResultActions;
 import reactor.core.publisher.Mono;
 
 import static com.bootme.util.fixture.PromptFixture.*;
+import static com.epages.restdocs.apispec.ResourceDocumentation.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,7 +43,34 @@ class PromptControllerTest extends ControllerTest {
         perform.andDo(print())
                 .andDo(document("prompts/generate/success",
                         preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint())));
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .summary("프롬프트 생성")
+                                .description("유저의 입력 값을 ChatGPT API에 전송하고 ChatGPT 입력에 최적화된 형태의 프롬프트를 반환한다.")
+                                .pathParameters(
+                                        parameterWithName("type").description("프롬프트 분류")
+                                )
+                                .requestFields(
+                                        fieldWithPath("promptType").description("프롬프트 분류"),
+                                        fieldWithPath("techStack").description("기술 스택"),
+                                        fieldWithPath("principles").description("관점 & 원칙"),
+                                        fieldWithPath("responseFormat").description("답변 형식"),
+                                        fieldWithPath("promptLanguage").description("프롬프트 언어"),
+                                        fieldWithPath("question").description("질문 내용").optional(),
+                                        fieldWithPath("example").description("예시").optional(),
+                                        fieldWithPath("featureDescription").description("기능 설명").optional(),
+                                        fieldWithPath("exampleCode").description("예시 코드").optional(),
+                                        fieldWithPath("apiDescription").description("API 설명").optional(),
+                                        fieldWithPath("erd").description("ERD").optional(),
+                                        fieldWithPath("entity").description("엔티티 or 모델 코드").optional()
+                                )
+                                // todo: 응답 타입 Mono 인 경우 응답 바디 있어도 없다고 뜨는 에러 있음
+                                // 참고: https://github.com/spring-projects/spring-restdocs/issues/564#issuecomment-441003527
+//                                .responseFields(
+//                                        fieldWithPath("generatedPrompt").description("생성된 프롬프트")
+//                                )
+                                .build())
+                ));
     }
 
     @Test
@@ -62,7 +92,20 @@ class PromptControllerTest extends ControllerTest {
         perform.andDo(print())
                 .andDo(document("prompts/submit/success",
                         preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint())));
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .summary("프롬프트 입력")
+                                .description("`프롬프트 생성`을 통해 생성된 프롬프트를 ChatGPT API에 전송하고 응답값을 반환한다.")
+                                .requestFields(
+                                        fieldWithPath("prompt").description("프롬프트")
+                                )
+                                // todo: 응답 타입 Mono 인 경우 응답 바디 있어도 없다고 뜨는 에러 있음
+                                // 참고: https://github.com/spring-projects/spring-restdocs/issues/564#issuecomment-441003527
+//                                .responseFields(
+//                                        fieldWithPath("generatedPrompt").description("생성된 프롬프트")
+//                                )
+                                .build())
+                ));
     }
 
 }
