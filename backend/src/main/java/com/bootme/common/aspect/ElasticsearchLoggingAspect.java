48a963fe9a6a7749f8e26b2aae7aa43f647297cc
Jinwook Kim <kimjinwook94@gmail.com>
2023-11-04 19:54:43 +0900

    [BE] Elasticsearch 로깅 추가

---
+++ backend/src/main/java/com/bootme/common/aspect/ElasticsearchLoggingAspect.java
@@ -0,0 +1,46 @@
+package com.bootme.common.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@Aspect
+@Component
+@Slf4j
+public class ElasticsearchLoggingAspect {
+
+    @Around("execution(* com.bootme.post.repository.PostElasticsearchRepository.*(..))")
+    public Object logRepositoryMethodExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+            return result;
+        } finally {
+            stopWatch.stop();
+            String logMessage = String.format(
+                    "| %d ms | %s",
+                    stopWatch.getTotalTimeMillis(),
+                    getCallingMethodDetails()
+            );
+            log.info(logMessage);
+        }
+    }
+
+    private String getCallingMethodDetails() {
+        Optional<StackTraceElement> stackTraceElement = Arrays.stream(Thread.currentThread().getStackTrace())
+                .filter(e -> e.getClassName().startsWith("com.bootme") && e.getClassName().endsWith("Service"))
+                .findFirst();
+
+        return stackTraceElement.map(StackTraceElement::toString).orElse("Unknown method");
+    }
+
+}
