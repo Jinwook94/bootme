4e4151d099e09e70e263e638c0b0baabede61787
Jinwook94 <kimjinwook94@gmail.com>
2023-12-31 02:10:51 +0900

    [BE] Secrets Manager Credential 입력 방식 변경

--- backend/src/main/java/com/bootme/config/AWSSecretsManagerConfig.java
+++ backend/src/main/java/com/bootme/config/AWSSecretsManagerConfig.java
@@ -1,6 +1,7 @@
 package com.bootme.config;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.secretsmanager.caching.SecretCache;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
@@ -10,6 +11,7 @@ import com.bootme.common.exception.SerializationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,11 +23,17 @@ import static com.bootme.common.exception.ErrorType.JSON_PROCESSING_FAIL;
 @Configuration
 public class AWSSecretsManagerConfig {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
     @Bean
     public AWSSecretsManager awsSecretsManager() {
         return AWSSecretsManagerClientBuilder.standard()
                 .withRegion(Regions.AP_NORTHEAST_2)
-                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
                 .build();
     }
 
