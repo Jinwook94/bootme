84aaa9f87ba458af537a4652167a320702f2e71f
Jinwook94 <kimjinwook94@gmail.com>
2025-03-30 23:07:50 +0900

    [BE] elasticsearch 설정 개선

--- backend/src/main/java/com/bootme/config/ElasticsearchConfig.java
+++ backend/src/main/java/com/bootme/config/ElasticsearchConfig.java
@@ -10,23 +10,30 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackages = "com.bootme.post.repository")
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
-    @Value("${elasticsearch.host:10.0.6.174}")
+    // elasticsearch.host 정의되어 있으면 그 값을 사용하고, 정의되어 있지 않으면 localhost 사용
+    @Value("${elasticsearch.host:localhost}")
     private String elasticsearchHost;
 
     @Value("${elasticsearch.port:9200}")
     private int elasticsearchPort;
 
-    @Value("${elasticsearch.username}")
+    @Value("${elasticsearch.username:}")
     private String elasticsearchUsername;
 
-    @Value("${elasticsearch.password}")
+    @Value("${elasticsearch.password:}")
     private String elasticsearchPassword;
 
     @Override
     public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo(elasticsearchHost + ":" + elasticsearchPort)
-                .withBasicAuth(elasticsearchUsername, elasticsearchPassword)
-                .build();
+        ClientConfiguration.MaybeSecureClientConfigurationBuilder builder = ClientConfiguration.builder()
+                .connectedTo(elasticsearchHost + ":" + elasticsearchPort);
+
+        // 사용자 이름과 비밀번호가 모두 설정된 경우에만 기본 인증 추가
+        if (elasticsearchUsername != null && !elasticsearchUsername.isEmpty() &&
+                elasticsearchPassword != null && !elasticsearchPassword.isEmpty()) {
+            builder.withBasicAuth(elasticsearchUsername, elasticsearchPassword);
+        }
+
+        return builder.build();
     }
 }
